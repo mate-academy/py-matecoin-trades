2beaacea17d86b27e85d93cf7ae81e981c8fec9f
Oleksandr Zhukov <alexanderzhukov88@gmail.com>
2022-02-07 17:48:28 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -11,12 +11,23 @@ def calculate_profit(trades_file):
             if data[key]:
                 data[key] = Decimal(data[key])
 
-    bought_sum = sum([val["bought"] for val in users if val["bought"]])
-    sold_sum = sum([val["sold"] for val in users if val["sold"]])
+    # two variables for sums are created for better readability
+    bought_sum = sum(
+        [val["bought"] for val in users if val["bought"]]
+    )
+    sold_sum = sum(
+        [val["sold"] for val in users if val["sold"]]
+    )
     matecoin_total = bought_sum - sold_sum
 
-    bought_money_sum = sum([val["bought"] * val["matecoin_price"] for val in users if val["bought"]])
-    sold_money_sum = sum([val["sold"] * val["matecoin_price"] for val in users if val["sold"]])
+    bought_money_sum = sum(
+        [val["bought"] * val["matecoin_price"]
+         for val in users if val["bought"]]
+    )
+    sold_money_sum = sum(
+        [val["sold"] * val["matecoin_price"]
+         for val in users if val["sold"]]
+    )
     earned_total = bought_money_sum - sold_money_sum
 
     profit_json = {
