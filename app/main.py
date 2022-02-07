d04ab758f1f58efa2a85602f5196f8df9d8d7cee
Vlad Tsvigun <vl.tsv@outlook.com>
2022-02-07 17:29:30 +0200

    Solutino

--- app/main.py
+++ app/main.py
@@ -1,1 +1,34 @@
-# write your code here
+import json
+import decimal
+
+
+def calculate_profit(file_name):
+
+    earned_money = 0
+    matecoin_account = 0
+
+    with open(file_name, "r") as json_data:
+        data = json.load(json_data)
+
+        for trade in data:
+            if not trade["bought"]:
+                trade["bought"] = 0
+            if not trade["sold"]:
+                trade["sold"] = 0
+
+            earned_money +=\
+                (decimal.Decimal(trade["bought"])
+                 - decimal.Decimal(trade["sold"])) \
+                * decimal.Decimal(trade["matecoin_price"])
+
+            matecoin_account += (decimal.Decimal(trade["bought"])
+                                 - decimal.Decimal(trade["sold"]))
+
+    profit_dict = {"earned_money": f"{earned_money}",
+                   "matecoin_account": f"{matecoin_account}"}
+
+    with open("profit.json", "w") as profit:
+        json.dump(profit_dict, profit, indent=2)
+
+
+calculate_profit("trades.json")
