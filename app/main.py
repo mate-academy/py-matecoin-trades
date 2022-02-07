65c7fd95627a31aba188700817ced35f63f3dcc2
alix makarova <alix.makarova@gmail.com>
2022-02-07 15:06:49 +0200

    Replaced float with decimal

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import decimal
 import json
 
 
@@ -10,15 +11,15 @@ def calculate_profit(name_of_file: str):
         "matecoin_account": 0
     }
     for trade in trades_data:
-        price = float(trade["matecoin_price"])
+        price = decimal.Decimal(trade["matecoin_price"])
         if trade["bought"] and trade["sold"]:
-            bought = float(trade["bought"])
-            sold = float(trade["sold"])
+            bought = decimal.Decimal(trade["bought"])
+            sold = decimal.Decimal(trade["sold"])
         elif not trade["bought"] and trade["sold"]:
             bought = 0
-            sold = float(trade["sold"])
+            sold = decimal.Decimal(trade["sold"])
         elif not trade["sold"] and trade["bought"]:
-            bought = float(trade["bought"])
+            bought = decimal.Decimal(trade["bought"])
             sold = 0
 
         profit_data["matecoin_account"] += bought - sold
