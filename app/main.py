485bbb2bd6312ed21ff61a731e76505e7db4342f
TJgkOsVP <sasha06052001@gmail.com>
2022-02-07 15:05:50 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -9,16 +9,13 @@ def calculate_profit(file_name):
     with open(file_name) as source:
         trades_information = json.load(source)
 
-    for trade_information in trades_information:
-        if not trade_information["sold"]:
-            trade_information["sold"] = 0
-        elif not trade_information["bought"]:
-            trade_information["bought"] = 0
-        trade_profit = \
-            Decimal(trade_information
-                    ["sold"]) - Decimal(trade_information["bought"])
-        earned_money += \
-            Decimal(trade_information["matecoin_price"]) * trade_profit
+    for trade in trades_information:
+        if not trade["sold"]:
+            trade["sold"] = 0
+        elif not trade["bought"]:
+            trade["bought"] = 0
+        trade_profit = Decimal(trade["sold"]) - Decimal(trade["bought"])
+        earned_money += Decimal(trade["matecoin_price"]) * trade_profit
         matecoin_account += -1 * trade_profit
 
     with open("profit.json", "a") as destination:
