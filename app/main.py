9bd16db2e9d472d6b5eba3b967e61f92e9b20bd8
Denys Lipin <lipin.transstream@gmail.com>
2022-02-08 13:09:18 +0200

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -5,21 +5,18 @@ from decimal import Decimal
 def calculate_profit(name: str):
     with open(name) as file_read:
         trades_data = json.load(file_read)
-    money_dict = {}
+    money_account = Decimal("0")
     coin_account = Decimal("0")
     for trade in trades_data:
-        if trade["sold"] is None:
-            money_bought = Decimal(trade["bought"]) * \
+        if trade["bought"]:
+            money_account -= Decimal(trade["bought"]) * \
                 Decimal(trade["matecoin_price"])
-            money_dict["bought"] = money_dict.get("bought", 0) + money_bought
             coin_account += Decimal(trade["bought"])
-        if trade["bought"] is None:
-            money_sold = Decimal(trade["sold"]) * \
+        if trade["sold"]:
+            money_account += Decimal(trade["sold"]) * \
                 Decimal(trade["matecoin_price"])
-            money_dict["sold"] = money_dict.get("sold", 0) + money_sold
             coin_account -= Decimal(trade["sold"])
-    earned_money = money_dict["sold"] - money_dict["bought"]
-    result = {"earned_money": str(earned_money),
+    result = {"earned_money": str(money_account),
               "matecoin_account": str(coin_account)}
     with open("profit.json", "w") as file_write:
         json.dump(result, file_write, indent=1)
