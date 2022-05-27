6d8221b6db803cc51cda80604b24ff6828712292
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-05-27 10:17:46 +0300

    Fix remarks. Ref code

--- app/main.py
+++ app/main.py
@@ -2,35 +2,29 @@ from decimal import Decimal
 from json import dump, load
 
 
-def calculate_profit(trade_history):
+def calculate_profit(file):
+    with open(file, "r") as trade_file:
+        trade_history = load(trade_file)
+
     sum_profit = 0
+    matecoin_account = 0
+
     for trade in trade_history:
         price = Decimal(trade["matecoin_price"])
         if trade["bought"]:
             sum_profit += Decimal(trade["bought"]) * price
+            matecoin_account += Decimal(trade["bought"])
         if trade["sold"]:
             sum_profit -= Decimal(trade["sold"]) * price
-    return sum_profit
-
-
-def calculate_balance_account(trade_history):
-    matecoin_account = 0
-    for item in trade_history:
-        if item["bought"]:
-            matecoin_account += Decimal(item["bought"])
-        if item["sold"]:
-            matecoin_account -= Decimal(item["sold"])
-    return matecoin_account
-
-
-if __name__ == "__main__":
-    with open("trades.json", "r") as trade_file:
-        trade_history = load(trade_file)
+            matecoin_account -= Decimal(trade["sold"])
 
     result = {
-        "earned_money": str(calculate_profit(trade_history)),
-        "matecoin_account": str(calculate_balance_account(trade_history))
+        "earned_money": str(sum_profit),
+        "matecoin_account": str(matecoin_account)
     }
 
     with open("profit.json", "w") as js_file:
         dump(result, js_file)
+
+
+calculate_profit("trades.json")
