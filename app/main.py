9ca5030e5c46142a77d8edfb252647b01453e2e9
Flashmobber <ufinaid@gmail.com>
2022-06-06 19:13:41 +0300

    All done.

--- app/main.py
+++ app/main.py
@@ -10,11 +10,11 @@ class Trades:
         self.bought = bought
         self.sold = sold
         self.matecoin_price = matecoin_price
-        if self.bought is not None:
+        if self.bought:
             self.earned_money -= Decimal(self.bought) * Decimal(
                 self.matecoin_price)
             self.matecoin_account += Decimal(self.bought)
-        if self.sold is not None:
+        if self.sold:
             self.earned_money += Decimal(self.sold) * Decimal(
                 self.matecoin_price)
             self.matecoin_account -= Decimal(self.sold)
@@ -34,6 +34,10 @@ def calculate_profit(file_name: str):
                 -1].earned_money
             trades_stat_dict["matecoin_account"] += trades_stat_list[
                 -1].matecoin_account
+
+    trades_stat_dict["earned_money"] = str(trades_stat_dict["earned_money"])
+    trades_stat_dict["matecoin_account"] = str(
+        trades_stat_dict["matecoin_account"])
 
     with open("profit.json", "w") as f:
         json.dump(trades_stat_dict, f, indent=2)
