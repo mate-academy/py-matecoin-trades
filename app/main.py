5dd4a376b47d286a76fd1f3d33fb33e5a4fde02f
Roma Tsybulia <Romalonstejn@gmail.com>
2022-06-07 21:05:08 +0300

    Change type of items of 'profit' dict

--- app/main.py
+++ app/main.py
@@ -6,7 +6,6 @@ def calculate_profit(file_name: str):
     with open(file_name, "r") as t:
         trades = json.load(t)
 
-    print(trades)
     profit = {"earned_money": 0, "matecoin_account": 0}
 
     for account in trades:
@@ -15,13 +14,13 @@ def calculate_profit(file_name: str):
                 Decimal(account["bought"]) * Decimal(account["matecoin_price"])
             profit["matecoin_account"] += Decimal(account["bought"])
 
-        elif account["sold"]:
+        else:
             profit["earned_money"] -= \
                 Decimal(account["sold"]) * Decimal(account["matecoin_price"])
             profit["matecoin_account"] -= Decimal(account["sold"])
 
-    profit["earned_money"] = float(profit["earned_money"])
-    profit["matecoin_account"] = float(profit["matecoin_account"])
+    profit["earned_money"] = str(profit["earned_money"])
+    profit["matecoin_account"] = str(profit["matecoin_account"])
 
     with open("profit.json", "w") as f:
         json.dump(profit, f, indent=2)
