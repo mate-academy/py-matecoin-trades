41111ba79b453ac4fb76928885354b1c4a21af1e
Serhii Cheshko <cheshko.sergey@gmail.com>
2022-06-07 09:52:59 +0300

    'matecoin-trades'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-# write your code here
+import json
+from decimal import Decimal
+
+
+def calculate_profit(trade_info: str):
+    earned_money = Decimal()
+    matecoin_account = Decimal()
+
+    with open(trade_info, "r") as f, open("profit.json", "w") as g:
+        transactions = json.load(f)
+
+        for transaction in transactions:
+            if transaction["bought"]:
+                earned_money -= Decimal(transaction["bought"]) * \
+                    Decimal(transaction["matecoin_price"])
+                matecoin_account += Decimal(transaction["bought"])
+            else:
+                earned_money += Decimal(transaction["sold"]) * \
+                    Decimal(transaction["matecoin_price"])
+                matecoin_account -= Decimal(transaction["sold"])
+
+        info = {"earned_money": str(earned_money),
+                "matecoin_account": str(matecoin_account)}
+
+        json.dump(info, g, indent=2)
