a977706b5000cff476b63cc6ae64342488c25d8c
Flashmobber <ufinaid@gmail.com>
2022-06-07 17:32:57 +0300

    comments acepted

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import json
 from decimal import Decimal, getcontext
 
 
-class Trades:
+class Trade:
     earned_money = Decimal(0)
     matecoin_account = Decimal(0)
 
@@ -24,16 +24,15 @@ def calculate_profit(file_name: str):
     getcontext().prec = 8
     trades_stat_dict = {"earned_money": 0,
                         "matecoin_account": 0}
-    trades_stat_list = []
+
     with open(file_name, "r") as f:
         for operation in json.load(f):
-            trades_stat_list.append(Trades(operation["bought"],
-                                           operation["sold"],
-                                           operation["matecoin_price"]))
-            trades_stat_dict["earned_money"] += trades_stat_list[
-                -1].earned_money
-            trades_stat_dict["matecoin_account"] += trades_stat_list[
-                -1].matecoin_account
+            trades_stat_list = Trade(operation["bought"],
+                                     operation["sold"],
+                                     operation["matecoin_price"])
+            trades_stat_dict["earned_money"] += trades_stat_list.earned_money
+            trades_stat_dict[
+                "matecoin_account"] += trades_stat_list.matecoin_account
 
     trades_stat_dict["earned_money"] = str(trades_stat_dict["earned_money"])
     trades_stat_dict["matecoin_account"] = str(
