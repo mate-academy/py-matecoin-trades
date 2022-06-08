3eaa7eebb90e9b739d814db5bb4a6e9851bfbaa1
Evgeniy Tretiak <103074600+Evgeniy994@users.noreply.github.com>
2022-06-08 14:53:59 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,13 +11,13 @@ def calculate_profit(file_name):
     matecoin_account = Decimal("0")
 
     for trade in trades:
-        if trade["sold"]:
+                if trade["sold"]:
             earned_money += Decimal(trade["sold"]) * \
-            Decimal(trade["matecoin_price"])
+                            Decimal(trade["matecoin_price"])
             matecoin_account -= Decimal(trade["sold"])
         else:
             earned_money -= Decimal(trade["sold"]) * \
-            Decimal(trade["matecoin_price"])
+                            Decimal(trade["matecoin_price"])
             matecoin_account += Decimal(trade["sold"])
 
     result = {
