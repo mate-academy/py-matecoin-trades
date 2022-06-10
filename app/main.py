a8c00080853041115d372be9f584d4687fdaf4bd
Artem Kharchenko <darten360@gmail.com>
2022-06-10 16:48:11 +0300

    All done

--- app/main.py
+++ app/main.py
@@ -5,27 +5,18 @@ from decimal import Decimal
 def calculate_profit(name: str):
     with open(name) as f:
         data = json.load(f)
-        bought = 0
-        sold = 0
-        bought_account = 0
-        sold_account = 0
+        money = Decimal("0")
+        account = Decimal("0")
         for trans in data:
             if trans["bought"] is not None:
-                bought += Decimal(trans["bought"])
-                bought_account += \
-                    Decimal(trans["bought"]) * \
-                    Decimal(trans["matecoin_price"])
-            bought += 0
+                money += Decimal(trans["bought"])
+                account += (Decimal(trans["bought"]) *
+                            Decimal(trans["matecoin_price"]))
 
             if trans["sold"] is not None:
-                sold += Decimal(trans["sold"])
-                sold_account += \
-                    Decimal(trans["sold"]) * \
-                    Decimal(trans["matecoin_price"])
-            sold += 0
-
-            money = bought - sold
-            account = bought_account - sold_account
+                money -= Decimal(trans["sold"])
+                account -= (Decimal(trans["sold"]) *
+                            Decimal(trans["matecoin_price"]))
 
         profit = {
             "earned_money": str(money),
