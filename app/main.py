2bba49684d3ee9caa997cfe10d8abd7c0885a80f
Anton Lasko <anton.lasko1993@gmail.com>
2022-06-17 16:59:21 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,20 +1,21 @@
 import json
+import decimal
 
 
 def calculate_profit():
     profit = {"earned_money": 0,
               "matecoin_account": 0}
-    with open("trades.json") as f:
-        data = json.load(f)
-        for new_dict in data:
-            if new_dict["bought"] != 'null':
-                profit["matecoin_account"] += new_dict["bought"]
-                profit["earned_money"] -= new_dict["bought"] *\
-                    new_dict["matecoin_price"]
-            if new_dict["sold"] != 'null':
+    with open("trades.json") as json_file:
+        data = json.load(json_file)
+        for my_dic in data:
+            if my_dic["bought"] != 'null':
+                profit["matecoin_account"] += decimal.Decimal(my_dic["bought"])
+                profit["earned_money"] -= decimal.Decimal(my_dic["bought"]) *\
+                    decimal.Decimal(my_dic["matecoin_price"])
+            if my_dic["sold"] != 'null':
                 profit["matecoin_account"] -= \
-                    new_dict["sold"]
-                profit["earned_money"] += new_dict["sold"] *\
-                    new_dict["matecoin_price"]
-    with open("profit.json", "w") as f:
-        json.dump(profit, f, indent=2)
+                    decimal.Decimal(my_dic["sold"])
+                profit["earned_money"] += decimal.Decimal(my_dic["sold"]) *\
+                    decimal.Decimal(my_dic["matecoin_price"])
+    with open("profit.json", "w") as new_json_file:
+        json.dump(profit, new_json_file, indent=2)
