9054301899eab273393d8007d777c437369f11e9
Anastasiia Shevchenko <106030737+shevchenko-anastasiia@users.noreply.github.com>
2022-08-02 11:35:27 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,22 +6,24 @@ def calculate_profit(file_name: str):
     with open(file_name, "r") as f:
         trades_data = json.load(f)
 
-    earned_money = 0
-    matecoin_account = 0
+    bought_usd = 0
+    sold_usd = 0
+    bought_coins = 0
+    sold_coins = 0
 
     for trade in trades_data:
-        if trade["bought"] is not None:
-            earned_money += decimal.Decimal(trade["bought"])\
+        if trade["bought"]:
+            bought_usd += decimal.Decimal(trade["bought"])\
                 * decimal.Decimal(trade["matecoin_price"])
-            matecoin_account += decimal.Decimal(trade["bought"])
-        if trade["sold"] is not None:
-            earned_money += decimal.Decimal(trade["sold"])\
+            bought_coins += decimal.Decimal(trade["bought"])
+        if trade["sold"]:
+            sold_usd += decimal.Decimal(trade["sold"])\
                 * decimal.Decimal(trade["matecoin_price"])
-            matecoin_account += decimal.Decimal(trade["sold"])
+            sold_coins += decimal.Decimal(trade["sold"])
 
     profit = {
-        "earned_money": str(earned_money),
-        "matecoin_account": str(matecoin_account)
+        "earned_money": str(sold_usd - bought_usd),
+        "matecoin_account": str(bought_coins - sold_coins)
     }
 
     with open("profit.json", "w") as f:
