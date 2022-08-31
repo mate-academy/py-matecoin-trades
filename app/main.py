df697413a0cce695a9363064ad864007259c58de
Volodymyr Panov <vpanov25@gmail.com>
2022-08-31 18:06:12 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,28 @@
-# write your code here
+import json
+import decimal
+
+
+profit = 0
+result = {
+    "earned_money": 0,
+    "matecoin_account": "0.00053"
+}
+
+with open("trades.json", "r") as file:
+    money = json.load(file)
+
+for day in money:
+    if day["bought"] is not None:
+        total_bought = decimal.Decimal(day["bought"])
+        bought_price = decimal.Decimal(day["matecoin_price"])
+        profit -= total_bought * bought_price
+    else:
+        total_sold = decimal.Decimal(day["sold"])
+        sold_price = decimal.Decimal(day["matecoin_price"])
+        profit += total_sold * sold_price
+
+
+result["earned_money"] = str(profit)
+
+with open("profit.json", "w") as f:
+    json.dump(result, f, indent=2)
