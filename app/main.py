f01eaa0ffbf7c12bd9344f37c7c002f41c7faefa
Paul Starodub <amorallex@gmail.com>
2022-09-12 23:24:56 +0300

    Solution)

--- app/main.py
+++ app/main.py
@@ -7,14 +7,14 @@ def calculate_profit(name_f):
     with open(name_f, "r") as f:
         data = json.load(f)
     earned_money, matecoin_account = 0, 0
-    for i in data:
-        change = decimal.Decimal(i["matecoin_price"])
-        if "bought" in i and i["bought"] is not None:
-            earned_money -= decimal.Decimal(i["bought"]) * change
-            matecoin_account += decimal.Decimal(i["bought"])
-        if "sold" in i and i["sold"] is not None:
-            earned_money += decimal.Decimal(i["sold"]) * change
-            matecoin_account -= decimal.Decimal(i["sold"])
+    for value in data:
+        change = decimal.Decimal(value["matecoin_price"])
+        if value["bought"]:
+            earned_money -= decimal.Decimal(value["bought"]) * change
+            matecoin_account += decimal.Decimal(value["bought"])
+        if value["sold"]:
+            earned_money += decimal.Decimal(value["sold"]) * change
+            matecoin_account -= decimal.Decimal(value["sold"])
     new_dict = {
         "earned_money": str(earned_money),
         "matecoin_account": str(matecoin_account)
