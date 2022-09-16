402e4fc81943c58182e769048cc9800452e1a6f1
Foggy-Cat <rosky.yaroslav@gmail.com>
2022-09-17 01:17:32 +0300

    Solution for trades

--- app/main.py
+++ app/main.py
@@ -8,12 +8,21 @@ def calculate_profit(name_file):
         trades = json.load(trades_file)
         profit = {"earned_money": "0", "matecoin_account": "0"}
         for trade in trades:
+            d = trade["matecoin_price"]
             if trade["bought"] is not None:
-                coin = str(Decimal(profit["matecoin_account"]) + Decimal(trade["bought"]))
+                matecoin = profit["matecoin_account"]
+                emoney = profit["earned_money"]
+                bght = trade["bought"]
+                coin = str(Decimal(matecoin) + Decimal(bght))
                 profit["matecoin_account"] = coin
-                money = str(Decimal(profit["earned_money"]) - Decimal(trade["matecoin_price"]) * Decimal(trade["bought"]))
+                money = str(Decimal(emoney) - Decimal(d) * Decimal(bght))
                 profit["earned_money"] = money
             if trade["sold"] is not None:
-                profit["matecoin_account"] = str(Decimal(profit["matecoin_account"]) - Decimal(trade["sold"]))
-                profit["earned_money"] = str(Decimal(profit["earned_money"]) + Decimal(trade["matecoin_price"]) * Decimal(trade["sold"]))
+                matecoin = profit["matecoin_account"]
+                emoney = profit["earned_money"]
+                sld = trade["sold"]
+                res_coin = str(Decimal(matecoin) - Decimal(sld))
+                profit["matecoin_account"] = res_coin
+                res_money = str(Decimal(emoney) + Decimal(d) * Decimal(sld))
+                profit["earned_money"] = res_money
         json.dump(profit, profit_file, indent=2)
