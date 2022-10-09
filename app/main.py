11929caafb1cec1968f9cc333770d3b6faa45ac3
Misha Doroshenko <doroshenko.misha73@gmail.com>
2022-10-09 18:09:38 +0300

    Fixed solution

--- app/main.py
+++ app/main.py
@@ -11,12 +11,14 @@ def calculate_profit(file_name: str) -> None:
                   "matecoin_account": decimal.Decimal("0")}
         for trade in trades:
             if isinstance(trade["bought"], str):
-                profit["earned_money"] -= (decimal.Decimal(trade["bought"]) *
-                                           decimal.Decimal(trade["matecoin_price"]))
+                profit["earned_money"] -= \
+                    decimal.Decimal(trade["bought"]) *\
+                    decimal.Decimal(trade["matecoin_price"])
                 profit["matecoin_account"] += decimal.Decimal(trade["bought"])
             if isinstance(trade["sold"], str):
-                profit["earned_money"] += (decimal.Decimal(trade["sold"]) *
-                                           decimal.Decimal(trade["matecoin_price"]))
+                profit["earned_money"] += \
+                    decimal.Decimal(trade["sold"]) *\
+                    decimal.Decimal(trade["matecoin_price"])
                 profit["matecoin_account"] -= decimal.Decimal(trade["sold"])
         profit["earned_money"] = str(profit["earned_money"])
         profit["matecoin_account"] = str(profit["matecoin_account"])
