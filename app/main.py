bd93334f69100e1e2f4b8a8eddf8e36cf70f3aef
Bohdan2001007 <bogdan1991007@gmail.com>
2022-10-18 14:37:16 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -10,15 +10,16 @@ def calculate_profit(file_name: str) -> None:
     res_matecoin_account = 0
     for symbol in trades_from_file:
         if symbol["bought"] is not None:
-            res_earned_money -= decimal.Decimal(symbol["bought"]) * decimal.Decimal(symbol["matecoin_price"])
+            res_earned_money -= decimal.Decimal(symbol["bought"]) *\
+                decimal.Decimal(symbol["matecoin_price"])
             res_matecoin_account += decimal.Decimal(symbol["bought"])
         if symbol["sold"] is not None:
-            res_earned_money += decimal.Decimal(symbol["sold"]) * decimal.Decimal(symbol["matecoin_price"])
+            res_earned_money += decimal.Decimal(symbol["sold"]) *\
+                decimal.Decimal(symbol["matecoin_price"])
             res_matecoin_account -= decimal.Decimal(symbol["sold"])
     dict_of_results = {
-  "earned_money": str(res_earned_money),
-  "matecoin_account": str(res_matecoin_account)
-}
+        "earned_money": str(res_earned_money),
+        "matecoin_account": str(res_matecoin_account)}
 
     with open("profit.json", "w") as new_file:
         json.dump(dict_of_results, new_file, indent=2)
