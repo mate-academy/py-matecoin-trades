841c2d1ff1071ca148dc9862539e69bc01ec5ced
Mazur Mariya <mazurinka29@gmail.com>
2022-12-06 00:12:46 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -29,6 +29,11 @@ def calculate_profit(name: str) -> None:
     profit_dict = {"earned_money": str(earned_money),
                    "matecoin_account": str(matecoin_account)}
 
-    with open("C:/Users/Expert/PycharmProjects/py-matecoin-trades"
-              "/profit.json", "w") as json_file:
+    with open("C:/Users/Expert/PycharmProjects/py-matecoin-trades/"
+              "profit.json", "w") as json_file:
         json.dump(profit_dict, json_file, indent=2)
+
+
+if __name__ == '__main__':
+    calculate_profit("C:/Users/Expert/PycharmProjects/py-matecoin-trades"
+                     "/app/trades.json")
