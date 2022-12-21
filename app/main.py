964d417819ce0e372ff83f989d02463291411da2
Ivan Berezyanskiy <65457374+Vanya2389@users.noreply.github.com>
2022-12-21 19:05:21 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,17 +9,18 @@ def calculate_profit(trade_file: str) -> None:
     }
     with open(trade_file, "r") as f:
         trades = json.load(f)
-    for i in trades:
-        if i["sold"]:
-            profit["earned_money"] += Decimal(i["sold"]) * Decimal(
-                i["mate-coin_price"])
-            profit["matecoin_account"] -= Decimal(i["sold"])
-        if i["bought"]:
-            profit["earned_money"] -= Decimal(i["bought"]) * Decimal(
-                i["matecoin_price"])
-            profit["matecoin_account"] += Decimal(i["bought"])
+        for i in trades:
+            if i["sold"]:
+                profit["earned_money"] += Decimal(i["sold"]) * Decimal(
+                    i["mate-coin_price"])
+                profit["matecoin_account"] -= Decimal(i["sold"])
+            if i["bought"]:
+                profit["earned_money"] -= Decimal(i["bought"]) * Decimal(
+                    i["matecoin_price"])
+                profit["matecoin_account"] += Decimal(i["bought"])
 
     profit["earned_money"] = str(profit["earned_money"])
     profit["matecoin_account"] = str(profit["matecoin_account"])
     with open("profit.json", "w") as f:
+        
         json.dump(profit, f, indent=2)
