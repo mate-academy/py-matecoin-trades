7ebd50a53487944c4f4a9c21dd79c6b161a6f1b4
Ivan Berezyanskiy <65457374+Vanya2389@users.noreply.github.com>
2022-12-21 18:58:57 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,21 +1,21 @@
 import json
-import decimal
+from decimal import Decimal as Decimal
 
 
 def calculate_profit(trade_file: str) -> None:
     profit = {
-        "earned_money": decimal.Decimal(0),
-        "matecoin_account": decimal.Decimal(0)
+        "earned_money": Decimal(0),
+        "matecoin_account": Decimal(0)
     }
     with open(trade_file, "r") as f:
         trades = json.load(f)
     for i in trades:
         if i["sold"]:
-            profit["earned_money"] += decimal.Decimal(i["sold"]) * decimal.Decimal(i["matecoin_price"])
-            profit["matecoin_account"] -= decimal.Decimal(i["sold"])
+            profit["earned_money"] += Decimal(i["sold"]) * Decimal(i["matecoin_price"])
+            profit["matecoin_account"] -= Decimal(i["sold"])
         if i["bought"]:
-            profit["earned_money"] -= decimal.Decimal(i["bought"]) * decimal.Decimal(i["matecoin_price"])
-            profit["matecoin_account"] += decimal.Decimal(i["bought"])
+            profit["earned_money"] -= Decimal(i["bought"]) * Decimal(i["matecoin_price"])
+            profit["matecoin_account"] += Decimal(i["bought"])
 
     profit["earned_money"] = str(profit["earned_money"])
     profit["matecoin_account"] = str(profit["matecoin_account"])
