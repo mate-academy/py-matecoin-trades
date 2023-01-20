e0223d264e77232ed975dc078b7d6ddb5157dc57
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-01-20 14:42:15 +0200

    Test1

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 
 
 def calculate_profit(name_file: str):
-    with open("app.trades.json", "r") as trade_info, open("app.profit.json", "w"):
+    with open(name_file, "r") as trade_info, open("app.profit.json", "w") as f:
         transactions_data = json.load(trade_info)
         matecoin_account = 0
         profit = 0
@@ -15,3 +15,5 @@ def calculate_profit(name_file: str):
 
             profit += (Decimal(trades["sold"]) - Decimal(trades["bought"])) * Decimal(trades["matecoint_price"])
             matecoin_account += Decimal(trades["bought"]) - Decimal(trades["sold"])
+        result = {"earned_money": str(profit), "matecoin_account": str(matecoin_account)}
+        json.dump(result, f, indent=2)
