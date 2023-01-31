7d5a48247d9b653c322d3ab452f9d7666a8e8053
Oleh Chornyi <ochernyi04@gmail.com>
2023-01-31 17:51:28 +0200

    Change path

--- app/main.py
+++ app/main.py
@@ -22,6 +22,5 @@ def calculate_profit(name_of_file: str) -> None:
         "matecoin_account": str(matecoin_account)
     }
 
-    path = "C:/Mate/py-matecoin-trades/" + "profit.json"
-    with open(path, "w") as file:
+    with open("app/profit.json", "w") as file:
         json.dump(profit, file, indent=2)
