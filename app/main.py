763a2db440c0cd2e9332f318a63bab4808b54f64
Oleh Chornyi <ochernyi04@gmail.com>
2023-01-31 19:56:04 +0200

    Change path again

--- app/main.py
+++ app/main.py
@@ -22,5 +22,5 @@ def calculate_profit(name_of_file: str) -> None:
         "matecoin_account": str(matecoin_account)
     }
 
-    with open("app/profit.json", "w") as file:
+    with open("profit.json", "w") as file:
         json.dump(profit, file, indent=2)
