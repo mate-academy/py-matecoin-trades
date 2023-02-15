c87dc16ae71ae6f44855e24b950abfa8ac9ff424
Oleksandr Lysanets <oleksandrlysanets@gmail.com>
2023-02-15 16:24:59 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,24 +5,24 @@ from decimal import Decimal
 def calculate_profit(file_name: str) -> None:
     with open(file_name, "r") as f:
         currency_data = json.load(f)
-    accumulation = {
+    trades = {
         "earned_money": Decimal(),
         "matecoin_account": Decimal()
     }
     for operation in currency_data:
         if operation["bought"] is not None:
-            accumulation["earned_money"] -= (
+            trades["earned_money"] -= (
                 Decimal(operation["bought"])
                 * Decimal(operation["matecoin_price"])
             )
-            accumulation["matecoin_account"] += Decimal(operation["bought"])
+            trades["matecoin_account"] += Decimal(operation["bought"])
         if operation["sold"] is not None:
-            accumulation["earned_money"] += (
+            trades["earned_money"] += (
                 Decimal(operation["sold"])
                 * Decimal(operation["matecoin_price"])
             )
-            accumulation["matecoin_account"] -= Decimal(operation["sold"])
-    accumulation["earned_money"] = str(accumulation["earned_money"])
-    accumulation["matecoin_account"] = str(accumulation["matecoin_account"])
-    with open("PROFIT.json", "w") as f:
-        json.dump(accumulation, f, indent=2)
+            trades["matecoin_account"] -= Decimal(operation["sold"])
+    trades["earned_money"] = str(trades["earned_money"])
+    trades["matecoin_account"] = str(trades["matecoin_account"])
+    with open("profit.json", "w") as f:
+        json.dump(trades, f, indent=2)
