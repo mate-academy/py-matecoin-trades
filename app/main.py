b4c92430718f8b99ede7625968e12b5e1fb52950
Maksym Nikityuk <maksym71417@gmail.com>
2023-02-22 15:59:24 +0200

    Fixed

--- app/main.py
+++ app/main.py
@@ -3,12 +3,10 @@ import json
 import decimal
 
 
-def calculate_profit(trades: json) -> json:
-    with open(r"C:\\Users\\maksy\\"
-              r"py\\py-matecoin-trades"
-              r"/app/trades.json") as file:
+def calculate_profit(trades_json: json) -> json:
+    with open(trades_json) as file:
         trades = json.load(file)
-        print(trades)
+
     buy = 0
     sell = 0
     account = 0
@@ -27,6 +25,4 @@ def calculate_profit(trades: json) -> json:
     result_dict = {"earned_money": str(sell - buy),
                    "matecoin_account": str(account)}
     with open("profit.json", "w") as profit:
-        profit_file = json.dump(result_dict, profit, indent=2)
-
-    return profit_file
+        json.dump(result_dict, profit, indent=2)
