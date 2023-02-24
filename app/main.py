ac4d00de7d55f8e8bf2404f364f3c335a7c327ce
Nazar Hladaniuk <powerlolek46@gmail.com>
2023-02-24 21:35:04 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,36 @@
-# write your code here
+import json
+from decimal import Decimal
+
+
+def calculate_profit(file_name: str) -> None:
+    matecoin_account = Decimal("0.0")
+    earned_money = Decimal("0.0")
+    with open(file_name, "r") as file:
+        data = json.load(file)
+        for day_data in data:
+            if day_data["sold"] is None:
+                matecoin_account += Decimal(day_data["bought"])
+                earned_money -= Decimal(day_data["matecoin_price"]) * Decimal(
+                    day_data["bought"]
+                )
+            if day_data["bought"] is None:
+                matecoin_account -= Decimal(day_data["sold"])
+                earned_money += Decimal(day_data["matecoin_price"]) * Decimal(
+                    day_data["sold"]
+                )
+            if day_data["sold"] and day_data["bought"]:
+                matecoin_account += Decimal(day_data["bought"])
+                earned_money -= Decimal(day_data["matecoin_price"]) * Decimal(
+                    day_data["bought"]
+                )
+                matecoin_account -= Decimal(day_data["sold"])
+                earned_money += Decimal(day_data["matecoin_price"]) * Decimal(
+                    day_data["sold"]
+                )
+
+    result_dict = {
+        "earned_money": str(earned_money),
+        "matecoin_account": str(matecoin_account)
+    }
+    with open("profit.json", "w") as f:
+        json.dump(result_dict, f, indent=2)
