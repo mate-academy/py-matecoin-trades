de85a7c6ba944c96a79cf85d008f151f7163f1d8
Ruslan <noezyboy1@gmail.com>
2023-02-27 14:01:50 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -3,24 +3,24 @@ import json
 import os
 
 
-def calculate_profit(profit_file=None):
-    trades_file = 'trades.json'
+def calculate_profit(profit_file: None = None) -> None:
+    trades_file = "trades.json"
     if not os.path.exists(trades_file):
         print(f"Error: {trades_file} file not found.")
         return None
 
-    with open(trades_file, 'r') as f:
+    with open(trades_file, "r") as f:
         trades = json.load(f)
 
     earned_money = Decimal(0)
     matecoin_account = Decimal(0)
 
     for trade in trades:
-        amount = Decimal(trade['amount'])
-        price = Decimal(trade['price'])
-        fee = Decimal(trade['fee'])
+        amount = Decimal(trade["amount"])
+        price = Decimal(trade["price"])
+        fee = Decimal(trade["fee"])
 
-        if trade['type'] == 'buy':
+        if trade["type"] == "buy":
             cost = amount * price + fee
             matecoin_account += amount
             earned_money -= cost
@@ -30,14 +30,17 @@ def calculate_profit(profit_file=None):
             earned_money += revenue
 
     if profit_file is not None:
-        with open(profit_file, 'w') as f:
+        with open(profit_file, "w") as f:
             json.dump({
-                'earned_money': str(earned_money),
-                'matecoin_account': str(matecoin_account)
+                "earned_money": str(earned_money),
+                "matecoin_account": str(matecoin_account)
             }, f)
-        return {'earned_money': earned_money, 'matecoin_account': matecoin_account}
+        return {"earned_money": earned_money,
+                "matecoin_account": matecoin_account}
     else:
-        return {'earned_money': earned_money, 'matecoin_account': matecoin_account}
+        return {"earned_money": earned_money,
+                "matecoin_account": matecoin_account}
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     calculate_profit(profit_file="profit.json")
