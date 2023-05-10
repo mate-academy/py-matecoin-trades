15a6e04e5c4c5eb09eba7b362850e14201b0c6de
Vlad Tetruk <vladteteruk01@gmail.com>
2023-05-10 21:24:06 +0300

    fix/flake8

--- app/main.py
+++ app/main.py
@@ -10,13 +10,16 @@ def calculate_profit(file_name: str) -> None:
     matecoin_account = 0
     profit = 0
     for value in data:
-        if value['bought']:
-            profit += Decimal(value['bought']) * Decimal(value["matecoin_price"])
+        if value["bought"]:
+            profit += (Decimal(value["bought"])
+                       * Decimal(value["matecoin_price"]))
             matecoin_account += Decimal(value["bought"])
         if value["sold"]:
-            profit -= Decimal(value["sold"]) * Decimal(value["matecoin_price"])
+            profit -= (Decimal(value["sold"])
+                       * Decimal(value["matecoin_price"]))
             matecoin_account -= Decimal(value["sold"])
 
-    result = {"earned_money": str(-profit), "matecoin_account": str(matecoin_account)}
+    result = {"earned_money": str(-profit),
+              "matecoin_account": str(matecoin_account)}
     with open("profit.json", "w") as file:
         json.dump(result, file, indent=2)
