4381cc5b8457100deec90dd8fa210251853b16ec
Anton Piddubnyi <antonpiddubnyii@gmail.com>
2023-05-18 20:07:53 -0600

    fixed indent

--- app/main.py
+++ app/main.py
@@ -16,13 +16,13 @@ def calculate_profit(file_name: str) -> None:
         if trade["bought"] is not None:
             profit["matecoin_account"] += Decimal(trade["bought"])
             profit["earned_money"] -= (
-                    Decimal(trade["bought"]) * Decimal(trade["matecoin_price"])
+                Decimal(trade["bought"]) * Decimal(trade["matecoin_price"])
             )
 
         if trade["sold"] is not None:
             profit["matecoin_account"] -= Decimal(trade["sold"])
             profit["earned_money"] += (
-                    Decimal(trade["sold"]) * Decimal(trade["matecoin_price"])
+                Decimal(trade["sold"]) * Decimal(trade["matecoin_price"])
             )
 
     profit["matecoin_account"] = str(profit["matecoin_account"])
