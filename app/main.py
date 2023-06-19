22f5bbce6505e6b7ee18b269c72579a9f4628fdc
YOUR_NAME <YOUR_EMAIL>
2023-06-19 13:21:09 +0300

    Matecoin trades: First commit

--- app/main.py
+++ app/main.py
@@ -11,17 +11,17 @@ def calculate_profit(trades_file: str) -> None:
 
     for trade in trades_data:
         bought = Decimal(trade.get("bought") or "0")
-        sold = Decimal(trade.get("sold") or "0")
+        sold = Decimal(trade.get("sold", "0") or "0")
         matecoin_price = Decimal(trade.get("matecoin_price"))
 
         matecoin_account += bought - sold
         earned_money -= bought * matecoin_price
         earned_money += sold * matecoin_price
 
-    income_data = {
+    income_data_to_write = {
         "earned_money": str(earned_money),
         "matecoin_account": str(matecoin_account)
     }
 
     with open("profit.json", "w") as file:
-        json.dump(income_data, file, indent=2)
+        json.dump(income_data_to_write, file, indent=2)
