63d8988d546d647d0ab5132f3b87dd5aef700ac3
Illya Zhmurskiy <thebrawler6@gmail.com>
2023-07-28 18:34:01 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 import json
 from decimal import Decimal, ROUND_DOWN
-from typing import Any
 
 
-def calculate_profit(trades_file: Any) -> None:
+def calculate_profit(trades_file: json) -> None:
     with open(trades_file, "r") as f:
         trades_data = json.load(f)
 
@@ -11,21 +10,26 @@ def calculate_profit(trades_file: Any) -> None:
     matecoin_account = Decimal("0")
 
     for trade in trades_data:
+        bought = Decimal(trade["bought"]) if trade["bought"] else None
+        sold = Decimal(trade["sold"]) if trade["sold"] else None
         matecoin_price = Decimal(trade["matecoin_price"])
-        bought = Decimal(trade.get("bought", "0") or "0")
-        sold = Decimal(trade.get("sold", "0") or "0")
-        earned_money += sold * matecoin_price
-        matecoin_account += bought - sold
-    earned_money = earned_money.quantize(Decimal("1E-8"), rounding=ROUND_DOWN)
-    matecoin_account = matecoin_account.quantize(Decimal("1E-8"), rounding=ROUND_DOWN)
 
-    earned_money = earned_money.normalize()
-    matecoin_account = matecoin_account.normalize()
+        if bought is not None:
+            matecoin_account += bought
+            earned_money -= bought * matecoin_price
+        if sold is not None:
+            matecoin_account -= sold
+            earned_money += sold * matecoin_price
 
-    result = {
-        "earned_money": str(earned_money),
-        "matecoin_account": str(matecoin_account),
-    }
+    earned_money = earned_money.quantize(Decimal("0.0000000"),
+                                         rounding=ROUND_DOWN)
+    matecoin_account = matecoin_account.quantize(Decimal("0.00000"),
+                                                 rounding=ROUND_DOWN)
 
-    with open("profit.json", "w") as f:
-        json.dump(result, f, indent=2, sort_keys=True)
+    with open("profit.json", "w") as profit_json:
+        result = {
+            "earned_money": str(earned_money),
+            "matecoin_account": str(matecoin_account)
+        }
+
+        json.dump(result, profit_json, indent=2)
