5f1cf73ff03a869238841029337fbf7d297df4a9
NO19 <5079447@gmil.com>
2023-08-11 18:36:33 +0300

    "Solution"

--- app/main.py
+++ app/main.py
@@ -1,1 +1,29 @@
-# write your code here
+from decimal import Decimal
+import json
+
+
+def matecoin_wallet(trades_file_name: str) -> None:
+    with open(trades_file_name, "r") as trades_open:
+        trade_sessions = json.load(trades_open)
+
+    earned_money = Decimal("0")
+    matecoin_wallet = Decimal("0")
+
+    for trade in trade_sessions:
+        if trade.get("bought") is not None:
+            earned_money -= (Decimal(trade.get("bought"))
+                             * Decimal(trade.get("matecoin_price")))
+            matecoin_wallet += Decimal(trade.get("bought"))
+
+        if trade.get("sold") is not None:
+            earned_money += (Decimal(trade.get("sold"))
+                             * Decimal(trade.get("matecoin_price")))
+            matecoin_wallet -= Decimal(trade.get("sold"))
+
+    profit = {
+        "earned_money": str(earned_money),
+        "matecoin_account": str(matecoin_wallet)
+    }
+
+    with open("profit.json", "w") as profit_file:
+        json.dump(profit, profit_file, indent=2)
