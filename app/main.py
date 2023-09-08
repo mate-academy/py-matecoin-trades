3f9eff9e7ed148324f0911d3c9ad091e96b97434
WoyDiam <freedom.channel2003@gmail.com>
2023-09-08 13:47:48 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -37,7 +37,5 @@ def calculate_profit(
         "matecoin_account": str(matecoin_account)
     }
 
-    output_file = "\\" + output_file
-
     with open(output_file, "w") as f:
         json.dump(result, f, indent=2)
