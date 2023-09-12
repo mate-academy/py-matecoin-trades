0bc6eaccb6d1b8caf7171516b332f316990def07
Kosta Stepanjuk <stepanjukkosta@qmail.com>
2023-09-12 20:08:25 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,40 @@
-# write your code here
+import os
+import json
+from decimal import Decimal
+
+
+def calculate_profit(name_file: str) -> None:
+    sum_bought = Decimal("0")
+    money_bought = Decimal("0")
+    sum_sold = Decimal("0")
+    money_sold = Decimal("0")
+
+    with open(name_file) as f:
+        trader_data = json.load(f)
+
+    for elem in trader_data:
+        if elem["bought"] is not None:
+            sum_bought += Decimal(elem["bought"])
+            money_bought += sum_bought * Decimal(elem["matecoin_price"])
+
+        if elem["sold"] is not None:
+            sum_sold += Decimal(elem["sold"])
+            money_sold += sum_sold * Decimal(elem["matecoin_price"])
+
+    balance_money = money_sold - money_bought
+    balance_account = sum_bought - sum_sold
+
+    dic = {
+        "earned_money": str(balance_money),
+        "matecoin_account": str(balance_account)
+    }
+
+    path_file2 = os.path.join(os.getcwd(), "profit.json")
+
+    with open(path_file2, "w") as f2:
+        json.dump(dic, f2)
+
+
+path_file = os.path.join(os.getcwd(), "trades.json")
+
+calculate_profit(path_file)
