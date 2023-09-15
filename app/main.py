10ca1fefe48ac4ea64f2dd083d1279ca3c092f7a
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2023-09-15 22:59:00 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -1,32 +1,32 @@
-import decimal
 import json
+from decimal import Decimal
 
 
-def calculate_profit(name_of_file: str) -> None:
-    profit = 0
-    amount_of_crypto = 0
+def calculate_profit(trade_json: json) -> None:
+    result = {
+        "earned_money": "0",
+        "matecoin_account": "0"
+    }
+    with open(trade_json) as trade_file:
+        file_transaction = json.load(trade_file)
 
-    print(name_of_file)
-    with open(name_of_file, "r") as account_file:
-        for account in json.load(account_file):
-            if account["bought"] is None:
-                amount_of_crypto -= decimal.Decimal(account["sold"])
-                profit += (decimal.Decimal(account["sold"])
-                           * decimal.Decimal(account["matecoin_price"]))
-            elif account["sold"] is None:
-                amount_of_crypto += decimal.Decimal(account["bought"])
-                profit -= (decimal.Decimal(account["bought"])
-                           * decimal.Decimal(account["matecoin_price"]))
-            else:
-                amount_of_crypto += (decimal.Decimal(account["bought"])
-                                     - decimal.Decimal(account["sold"]))
-                profit -= ((decimal.Decimal(account["bought"])
-                            - decimal.Decimal(account["sold"]))
-                           * decimal.Decimal(account["matecoin_price"]))
-        result = {
-            "earned_money": str(profit),
-            "matecoin_account": str(amount_of_crypto)
-        }
-    with open("C:\\Users\\stepk\\PycharmProjects"
-              "\\py-matecoin-trades\\profit.json", "w") as profit_file:
+    for trans in file_transaction:
+        if trans["sold"] is None:
+            trans["sold"] = "0"
+        if trans["bought"] is None:
+            trans["bought"] = "0"
+        result["earned_money"] = str(
+            Decimal(result["earned_money"])
+            - Decimal(trans["bought"])
+            * Decimal(trans["matecoin_price"])
+            + Decimal(trans["sold"])
+            * Decimal(trans["matecoin_price"])
+        )
+        result["matecoin_account"] = str(
+            Decimal(result["matecoin_account"])
+            + Decimal(trans["bought"])
+            - Decimal(trans["sold"])
+        )
+
+    with open("profit.json", "w") as profit_file:
         json.dump(result, profit_file, indent=2)
