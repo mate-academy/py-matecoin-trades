db0089dec66ba56b517c0f6f94d8d5247aa8bf0b
Andrew <67094594+MishkaGAMII@users.noreply.github.com>
2023-10-15 13:01:29 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import json
 from decimal import Decimal
 
 
-def calculate_profit(filename: str) -> None:
+def calculate_profit(filename: json) -> None:
     earned_money = Decimal("0")
     matecoin_account = Decimal("0")
     with open(filename, "r") as file:
