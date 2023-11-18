be9d5db86727f9294bb1ce6a2332b95e5f38c354
Alex <oleksa.shcherbakov@gmail.com>
2023-11-18 15:31:37 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -10,14 +10,14 @@ def calculate_profit(file_name: str) -> None:
     matecoin_account = Decimal("0")
     for trade in trade_data:
         if trade["bought"] is not None:
-            earned_money -= (
-                    Decimal(trade["bought"]) * Decimal(trade["matecoin_price"]))
-            matecoin_account += Decimal(trade["bought"])
+            bought = Decimal(trade["bought"])
+            earned_money -= bought * Decimal(trade["matecoin_price"])
+            matecoin_account += bought
 
         if trade["sold"] is not None:
-            earned_money += (
-                    Decimal(trade["sold"]) * Decimal(trade["matecoin_price"]))
-            matecoin_account -= Decimal(trade["sold"])
+            sold = Decimal(trade["sold"])
+            earned_money += sold * Decimal(trade["matecoin_price"])
+            matecoin_account -= sold
 
     temp_dict = {
         "earned_money": str(earned_money),
