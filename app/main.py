51abf37bdc1c737c7fd62c1d89f1f773b39d7b9d
Alex <oleksa.shcherbakov@gmail.com>
2023-11-18 15:24:46 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -3,18 +3,20 @@ from decimal import Decimal
 
 
 def calculate_profit(file_name: str) -> None:
-    with open (file_name, "r") as file:
+    with open(file_name, "r") as file:
         trade_data = json.load(file)
 
     earned_money = Decimal("0")
     matecoin_account = Decimal("0")
     for trade in trade_data:
         if trade["bought"] is not None:
-            earned_money -= Decimal(trade["bought"]) * Decimal(trade["matecoin_price"])
+            earned_money -= (
+                    Decimal(trade["bought"]) * Decimal(trade["matecoin_price"]))
             matecoin_account += Decimal(trade["bought"])
 
         if trade["sold"] is not None:
-            earned_money += Decimal(trade["sold"]) * Decimal(trade["matecoin_price"])
+            earned_money += (
+                    Decimal(trade["sold"]) * Decimal(trade["matecoin_price"]))
             matecoin_account -= Decimal(trade["sold"])
 
     temp_dict = {
