429a7f25ab5b9aabb93e1790f81ba81a830b4ce3
Illya Maznitskiy <comercaleuros@gmail.com>
2023-11-20 20:27:33 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -7,29 +7,25 @@ def calculate_profit(name: str) -> None:
     sold_in_dollars = Decimal("0")
     matecoin_account = Decimal("0")
 
-    with open(f"{name}", "r") as trades:
-        trades_data = json.load(trades)
+    with open(name, "r") as trades_file:
+        trades_data = json.load(trades_file)
 
         for trade in trades_data:
-            bought_coin = trade["bought"]
-            sold_coin = trade["sold"]
-            matecoin_price = trade["matecoin_price"]
+            bought_coin = trade.get("bought", 0)
+            sold_coin = trade.get("sold", 0)
+            matecoin_price = trade.get("matecoin_price", 0)
 
             if bought_coin:
                 bought_in_dollars += (
                     Decimal(bought_coin)
                     * Decimal(matecoin_price)
                 )
+                matecoin_account += Decimal(bought_coin)
             if sold_coin:
-                sold_in_dollars += (
-                    Decimal(sold_coin)
-                    * Decimal(matecoin_price)
-                )
+                sold_in_dollars += Decimal(sold_coin) * Decimal(matecoin_price)
+                matecoin_account -= Decimal(sold_coin)
 
-            if bought_coin and sold_coin:
-                matecoin_account += Decimal(bought_coin) - Decimal(sold_coin)
-
-        earned_money = bought_in_dollars - sold_in_dollars
+    earned_money = bought_in_dollars - sold_in_dollars
 
     data_profit = {
         "earned_money": f"{-earned_money}",
