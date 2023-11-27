cf518267446e87fe51acdef795237939db8ee171
Marinel444 <380978907444m@gmail.com>
2023-11-27 14:31:26 +0200

    resolve flake8

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import json
 import decimal
 
 
-def calculate_profit(file_name: str):
+def calculate_profit(file_name: str) -> None:
     with open(file_name,) as f:
         trades = json.load(f)
 
@@ -10,14 +10,17 @@ def calculate_profit(file_name: str):
     coin = decimal.Decimal("0.0")
     for trade in trades:
         if trade["bought"] is not None:
-            money -= decimal.Decimal(trade["matecoin_price"]) * decimal.Decimal(trade["bought"])
+            money -= (decimal.Decimal(trade["matecoin_price"])
+                      * decimal.Decimal(trade["bought"]))
             coin += decimal.Decimal(trade["bought"])
         if trade["sold"] is not None:
-            money += decimal.Decimal(trade["matecoin_price"]) * decimal.Decimal(trade["sold"])
+            money += (decimal.Decimal(trade["matecoin_price"])
+                      * decimal.Decimal(trade["sold"]))
             coin -= decimal.Decimal(trade["sold"])
     data = {
         "earned_money": str(money),
         "matecoin_account": str(coin),
     }
-    with open("profit.json", 'w', encoding="utf-8") as f:
-        json.dump(data, f, indent=2)
+
+    with open("profit.json", "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=2)
