e6fdb9161b2e9a06e2cba6addedb6ec4c90b3203
Viktor Rura <ruravictor@gmail.com>
2023-12-08 17:48:38 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -2,39 +2,24 @@ import json
 from decimal import Decimal
 
 
-def calculate_profit(file_path: str) -> None:
-
-    with open("/Users/rurakite/PycharmProjects/"
-              "py-matecoin-trades/app/trades.json", "r") as f:
-        trades = json.load(f)
-    sold = Decimal("0.0")
-    bought = Decimal("0.0")
-    sold_coins_amount = 0
-    bought_coins_amount = 0
-
-    for trade in trades:
-        if trade["sold"]:
-            sold += (
-                Decimal(trade["sold"]) * Decimal(trade["matecoin_price"])
-            )
-            sold_coins_amount += Decimal(trade["sold"])
-
-        if trade["bought"]:
-            bought += (
-                Decimal(trade["bought"]) * Decimal(trade["matecoin_price"])
-            )
-            bought_coins_amount += Decimal(trade["bought"])
-
-    earned_money = sold - bought
-    matecoin_account = bought_coins_amount - sold_coins_amount
-
-    result = {
-        "earned_money": str(earned_money),
-        "matecoin_account": str(matecoin_account)
+def calculate_profit(json_file: str) -> None:
+    with open(json_file, "r") as json_data:
+        trade_data = json.load(json_data)
+    count_money = Decimal(0)
+    count_matecion = Decimal(0)
+    for data_dict in trade_data:
+        matecoin_price = Decimal(data_dict["matecoin_price"])
+        if data_dict["bought"]:
+            bought = Decimal(data_dict["bought"])
+            count_money -= bought * matecoin_price
+            count_matecion += bought
+        if data_dict["sold"]:
+            sold = Decimal(data_dict["sold"])
+            count_money += sold * matecoin_price
+            count_matecion -= sold
+    result_dict = {
+        "earned_money": str(count_money),
+        "matecoin_account": str(count_matecion)
     }
-
-    with open("/Users/rurakite/PycharmProjects/"
-              "py-matecoin-trades/profit.json", "w") as output_file:
-        json.dump(result, output_file, indent=2)
-
-# calculate_profit()
+    with open("profit.json", "w") as result_file:
+        json.dump(result_dict, result_file, indent=2)
