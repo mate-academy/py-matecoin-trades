1063b3b2b7e32d95e54968b0c04f956a89c5f2ef
artem.puhach <artem.puhach@cyberpolice.gov.ua>
2024-01-01 14:43:31 +0200

    ready solution!

--- app/main.py
+++ app/main.py
@@ -1,31 +1,38 @@
 import json
+import os.path
 from decimal import Decimal
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-def calculate_profit(filename: str) -> None:
-
-    with open(filename, 'r') as data_file:
+def calculate_profit(
+        filepath: str = os.path.join(BASE_DIR, "app", "trades.json")
+) -> None:
+    with open(filepath, "r") as data_file:
         trades = json.load(data_file)
 
     earned_money = Decimal("0")
     matecoin_account = Decimal("0")
 
     for trade in trades:
-        bought = Decimal(trade.get('bought', 0) or 0)
-        sold = Decimal(trade.get('sold', 0) or 0)
-        matecoin_price = Decimal(trade['matecoin_price'])
+        bought = Decimal(trade.get("bought", 0) or 0)
+        sold = Decimal(trade.get("sold", 0) or 0)
+        matecoin_price = Decimal(trade.get("matecoin_price"))
 
         if bought > 0:
-            matecoin_account += bought
             earned_money -= bought * matecoin_price
-        elif sold > 0:
-            matecoin_account -= bought
-            earned_money += bought * matecoin_price
+            matecoin_account += bought
+        if sold > 0:
+            earned_money += sold * matecoin_price
+            matecoin_account -= sold
 
     result = {
         "earned_money": str(earned_money),
         "matecoin_account": str(matecoin_account)
     }
-
-    with open("profit.json", "w") as trade_result:
+    result_path = os.path.join(BASE_DIR, "profit.json")
+    with open(result_path, "w") as trade_result:
         json.dump(result, trade_result, indent=2)
+
+    return None
