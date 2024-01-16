1001f21fcbb7dc41903f71e13c5aab46891880fb
el chibuk <chebukin404@gmail.com>
2024-01-16 14:02:01 +0200

    removed excess lines

--- app/main.py
+++ app/main.py
@@ -1,28 +1,26 @@
 import json
-import decimal
+from decimal import Decimal
 
 
 def calculate_profit(json_file: str) -> None:
     with open(json_file, "r") as trades_file:
         trades = json.load(trades_file)
 
-    matecoin_account = 0
-    earned_money = 0
+    matecoin_account, earned_money = 0, 0
 
     for trade in trades:
-        matecoin_price = decimal.Decimal(trade["matecoin_price"])
         trade_amount = 0
 
         if trade["bought"] and trade["sold"]:
-            trade_amount = (decimal.Decimal(trade["bought"])
-                            - decimal.Decimal(trade["sold"]))
+            trade_amount = (Decimal(trade["bought"])
+                            - Decimal(trade["sold"]))
         elif trade["bought"]:
-            trade_amount = decimal.Decimal(trade["bought"])
+            trade_amount = Decimal(trade["bought"])
         elif trade["sold"]:
-            trade_amount = -decimal.Decimal(trade["sold"])
+            trade_amount = -Decimal(trade["sold"])
 
         matecoin_account += trade_amount
-        earned_money -= matecoin_price * trade_amount
+        earned_money -= Decimal(trade["matecoin_price"]) * trade_amount
 
     profit_calculations = {
         "earned_money": str(earned_money),
