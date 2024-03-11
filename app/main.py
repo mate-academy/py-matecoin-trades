2a6f71253e48bade1a555e285d0720735f279264
Serhii Kravchenko <serhii.kravchenko.q@gmail.com>
2024-03-11 19:23:42 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,30 +1,40 @@
 import json
 from decimal import Decimal
-from pathlib import Path
 
 
-def calculate_profit(filename: str = "trades.json") -> None:
-    with open(filename, "r") as f:
-        trades = json.load(f, parse_float=Decimal)
+def calculate_profit(file_name: str) -> None:
+    with open(file_name) as f:
+        trades = json.load(f)
+        for deal in trades:
+            if deal["sold"] is None:
+                deal["sold"] = 0
+            if deal["bought"] is None:
+                deal["bought"] = 0
 
-    earned_money = Decimal("0")
-    matecoin_account = Decimal("0")
+        matecoin_account = 0
+        earned_money = 0
 
-    for trade in trades:
-        # Directly handle None cases and ensure Decimal is used without falling back to float
-        bought = Decimal(trade.get("bought") or "0")
-        sold = Decimal(trade.get("sold") or "0")
-        matecoin_price = Decimal(trade["matecoin_price"])
-        matecoin_account += bought - sold
-        earned_money += (sold - bought) * matecoin_price
+        for deal in trades:
+            matecoin_account += (
+                Decimal((deal["bought"]))
+                - Decimal((deal["sold"]))
+            )
+            earned_money += (
+                Decimal(deal["sold"])
+                * Decimal(deal["matecoin_price"])
+            )
+            matecoin_account -= Decimal(deal["sold"])
+            earned_money -= (
+                Decimal(deal["bought"])
+                * Decimal(deal["matecoin_price"])
+            )
+            matecoin_account += Decimal(deal["sold"])
 
-    result = {
-        "earned_money": str(earned_money.normalize()),
-        "matecoin_account": str(matecoin_account.normalize())
-    }
+        balance = {
+            "earned_money": str(earned_money),
+            "matecoin_account": str(matecoin_account)
+        }
 
-    current_directory = Path.cwd()
-    profit_file_path = current_directory / "profit.json"
-
-    with open(profit_file_path, "w) as f:
-        json.dump(result, f, indent=2)
+        print(balance)
+    with open("profit.json", "w") as f:
+        json.dump(balance, f, indent=2)
