11cfa11693829b759ef4b4cfd1ee9ac4b19a7753
Vitalii Taran <vitaliy.taran.1993@gmail.com>
2024-04-04 11:32:10 +0300

    Solution_3)))

--- app/main.py
+++ app/main.py
@@ -1,27 +1,27 @@
 import json
-from decimal import Decimal as Dec
+from decimal import Decimal
 
 
 def calculate_profit(file_name: str) -> None:
     with open(file_name, "r") as file:
         trades_data = json.load(file)
 
-    profit = {
+    prof = {
         "earned_money": "0",
         "matecoin_account": "0"
     }
 
     for des in trades_data:
         if des["bought"] is not None:
-            cost_b = Dec(des["bought"]) * Dec(des["matecoin_price"])
-            profit["earned_money"] = str(Dec(profit["earned_money"]) - cost_b)
-            profit["matecoin_account"] = str(Dec(profit["matecoin_account"])
-                                             + Dec(des["bought"]))
+            cost_b = Decimal(des["bought"]) * Decimal(des["matecoin_price"])
+            prof["earned_money"] = str(Decimal(prof["earned_money"]) - cost_b)
+            prof["matecoin_account"] = str(Decimal(prof["matecoin_account"])
+                                           + Decimal(des["bought"]))
         if des["sold"] is not None:
-            cost_s = Dec(des["sold"]) * Dec(des["matecoin_price"])
-            profit["earned_money"] = str(Dec(profit["earned_money"]) + cost_s)
-            profit["matecoin_account"] = str(Dec(profit["matecoin_account"])
-                                             - Dec(des["sold"]))
+            cost_s = Decimal(des["sold"]) * Decimal(des["matecoin_price"])
+            prof["earned_money"] = str(Decimal(prof["earned_money"]) + cost_s)
+            prof["matecoin_account"] = str(Decimal(prof["matecoin_account"])
+                                           - Decimal(des["sold"]))
 
     with open("profit.json", "w") as file:
-        json.dump(profit, file, indent=2)
+        json.dump(prof, file, indent=2)
