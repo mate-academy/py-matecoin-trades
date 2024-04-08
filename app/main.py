2b5d6b219bd01b7a72ac770f3714df1012c2fa9a
Liliia <furman06228@gmail.com>
2024-04-09 01:02:58 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,36 @@
-# write your code here
+import json
+from decimal import Decimal
+import os
+
+
+def calculate_profit(trades_file: str) -> None:
+    with open(trades_file, "r") as f:
+        trades = json.load(f)
+
+    earned_money = Decimal("0")
+    matecoin_account = Decimal("0")
+
+    for trade in trades:
+        if trade["bought"] is not None:
+            bought = Decimal(trade["bought"])
+            matecoin_price = Decimal(trade["matecoin_price"])
+            cost = bought * matecoin_price
+            earned_money -= cost
+            matecoin_account += bought
+        if trade["sold"] is not None:
+            sold = Decimal(trade["sold"])
+            matecoin_price = Decimal(trade["matecoin_price"])
+            revenue = sold * matecoin_price
+            earned_money += revenue
+            matecoin_account -= sold
+
+    result = {
+        "earned_money": str(earned_money),
+        "matecoin_account": str(matecoin_account)
+    }
+
+    directory = r"C:\Users\prile\PycharmProjects\py-matecoin-trades1"
+    file_path = os.path.join(directory, "profit.json")
+
+    with open(file_path, "w") as f:
+        json.dump(result, f, indent=2)
