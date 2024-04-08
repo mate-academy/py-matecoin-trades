23af66e6d9e24905213b35fa26c0d40dd81f03dd
Futilite <marketesh@gmail.com>
2024-04-08 13:09:32 +0300

    solution_1

--- app/main.py
+++ app/main.py
@@ -1,15 +1,31 @@
-import random
+import json
+from decimal import Decimal
 
 
-def flip_coin(num_trials: int = 10000, num_flips: int = 10) -> dict:
-    heads_count = {i: 0 for i in range(num_flips + 1)}
+def calculate_profit(trades: str) -> None:
+    with open(trades, "r") as f:
+        fail_trades = json.load(f)
 
-    for _ in range(num_trials):
-        flips = [random.choice(["heads", "tails"]) for _ in range(num_flips)]
-        num_heads = flips.count("heads")
-        heads_count[num_heads] += 1
+    earned_money = Decimal("0")
+    matecoin_account = Decimal("0")
 
-    for heads in heads_count:
-        heads_count[heads] = round((heads_count[heads] / num_trials) * 100, 2)
+    for i in fail_trades:
+        bought_coin = Decimal(i.get("bought") or 0)
+        sold_coin = Decimal(i.get("sold") or 0)
+        matecoin_price = Decimal(i.get("matecoin_price") or 0)
 
-    return heads_count
+        if bought_coin > 0:
+            earned_money -= bought_coin * matecoin_price
+            matecoin_account += bought_coin
+
+        if sold_coin > 0:
+            earned_money += sold_coin * matecoin_price
+            matecoin_account -= sold_coin
+
+        result = {
+            "earned_money": str(earned_money),
+            "matecoin_account": str(matecoin_account)
+        }
+
+        with open("profit.json", "w") as output_file:
+            json.dump(result, output_file, indent=2)
