e31be01430adfc8a80936cea240aa5eed3c23429
bwaah1 <chykivan@gmail.com>
2024-04-10 18:28:52 +0300

    Implement calculate_profit function

--- app/main.py
+++ app/main.py
@@ -6,21 +6,22 @@ def calculate_profit(file_name: str) -> None:
     result = {
         "earned_money": Decimal("0"),
         "matecoin_account": Decimal("0")
-        }
+    }
 
     with open(file_name, "r") as file:
         trades = json.load(file)
 
         for transaction in trades:
-            amount_bought = Decimal(transaction["bought"]) if transaction["bought"] else Decimal("0")
-            amount_sold = Decimal(transaction["sold"]) if transaction["sold"] else Decimal("0")
+            amount_bought = (Decimal(transaction["bought"])
+                             if transaction["bought"] else Decimal("0"))
+            amount_sold = (Decimal(transaction["sold"])
+                           if transaction["sold"] else Decimal("0"))
             price = Decimal(transaction["matecoin_price"])
 
-            result["earned_money"] += (amount_sold * price) - (amount_bought * price)
+            result["earned_money"] += ((amount_sold * price)
+                                       - (amount_bought * price))
             result["matecoin_account"] += (amount_bought - amount_sold)
 
     with open("profit.json", "w") as file:
-        json.dump(result, file, ensure_ascii=False, default=str)
+        json.dump({k: str(v) for k, v in sorted(result.items())}, file)
 
-
-calculate_profit("trades.json")
