d716dd14cecdf7b861a9c3e961974788042a6b57
dvkim28 <d.villarionovich@gmail.com>
2024-04-18 01:33:12 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ from decimal import Decimal
 
 
 def calculate_profit(file_name: str) -> None:
-    total_bought = Decimal("")
-    total_sold = Decimal("")
-    matecoin_account = Decimal("")
+    total_bought = Decimal("0")
+    total_sold = Decimal("0")
+    matecoin_account = Decimal("0")
     with open(file_name, "r") as file:
         trades = json.load(file)
     for trade in trades:
