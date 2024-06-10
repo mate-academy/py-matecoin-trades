b8de0a5184be67e848bd6b6704b8c67cb332a1bd
Andrii1000 <127493083+Andrii1000@users.noreply.github.com>
2024-06-10 16:28:06 +0300

    Solution V2

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ import json
 from decimal import Decimal
 
 
-def calculate_profit(name_file: str) -> None:
-    with open(name_file, "r") as file:
+def calculate_profit(filename: str) -> None:
+    with open(filename, "r") as file:
         trades = json.load(file)
 
     total_spent = Decimal("0")
@@ -29,5 +29,5 @@ def calculate_profit(name_file: str) -> None:
         "matecoin_account": f"{matecoin_account:.5f}"
     }
 
-    with open(name_file, "w") as file_prof:
-        json.dump(profit, file_prof, indent=2)
+    with open("profit.json", "w") as file:
+        json.dump(profit, file, indent=2)
