4cc6603992dbb953a3a4fb6a42aa0894183b1d17
Maksym Protsak <maksym.protsak@gmail.com>
2024-06-19 23:10:51 +0300

    Solution 19-06-2024 23-10

--- app/main.py
+++ app/main.py
@@ -8,35 +8,48 @@ profit = {
 }
 
 
-def creat_sold(amount):
-    pass
+def prepare_decimal_dict_for_dump(dict_to_dump: dict) -> dict:
+    for key, value in dict_to_dump.items():
+        dict_to_dump[key] = str(value)
+    return dict_to_dump
 
 
-def creat_buy(amount):
-    pass
+def write_js_file(file_name: str, data: dict) -> None:
+    with open(file_name, "w") as file:
+        json.dump(data, file, indent=2)
+
+
+def creat_sold(amount: decimal, coin_price: decimal) -> None:
+    profit["earned_money"] += amount * coin_price
+    profit["matecoin_account"] -= amount
+
+
+def creat_buy(amount: decimal, coin_price: decimal) -> None:
+    profit["earned_money"] -= amount * coin_price
+    profit["matecoin_account"] += amount
 
 
 def create_transactions(actions: str) -> None:
     for action in actions:
         create_one_transaction(action)
-        break
 
 
 def create_one_transaction(action: str) -> None:
-    print(action)
     if action["bought"] is not None:
-        creat_buy(action["bought"])
+        creat_buy(action["bought"], action["matecoin_price"])
 
     if action["sold"] is not None:
-        creat_sold(action["sold"])
+        creat_sold(action["sold"], action["matecoin_price"])
 
 
 def prepare_js_file_after_read(js_file: str) -> str:
     for transaction in js_file:
         if transaction["bought"] is not None:
             transaction["bought"] = float_to_decimal(transaction["bought"])
-        elif transaction["sold"] is not None:
+        if transaction["sold"] is not None:
             transaction["sold"] = float_to_decimal(transaction["sold"])
+        transaction["matecoin_price"] = float_to_decimal(
+            transaction["matecoin_price"])
     return js_file
 
 
@@ -52,9 +65,7 @@ def read_js_file(file_name: str) -> str:
 
 def calculate_profit(file_name: str) -> None:
     js_file_as_list = read_js_file(file_name)
-    js_float_as_decimal = prepare_js_file_after_read(js_file_as_list)
-    create_transactions(js_float_as_decimal)
-
-
-if __name__ == "__main__":
-    calculate_profit("trades.json")
+    js_str_as_decimal = prepare_js_file_after_read(js_file_as_list)
+    create_transactions(js_str_as_decimal)
+    js_decimal_as_str = prepare_decimal_dict_for_dump(profit)
+    write_js_file("profit.json", js_decimal_as_str)
