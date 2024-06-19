1de10612d54bfd2de381465bb8dbe2f30514f6ee
Maksym Protsak <maksym.protsak@gmail.com>
2024-06-19 23:28:16 +0300

    Solution 19-06-2024 23:28

--- app/main.py
+++ app/main.py
@@ -1,11 +1,6 @@
+import os
 import json
 import decimal
-
-
-profit = {
-    "earned_money": decimal.Decimal(0),
-    "matecoin_account": decimal.Decimal(0)
-}
 
 
 def prepare_decimal_dict_for_dump(dict_to_dump: dict) -> dict:
@@ -19,27 +14,27 @@ def write_js_file(file_name: str, data: dict) -> None:
         json.dump(data, file, indent=2)
 
 
-def creat_sold(amount: decimal, coin_price: decimal) -> None:
+def creat_sold(amount: decimal, coin_price: decimal, profit: dict) -> None:
     profit["earned_money"] += amount * coin_price
     profit["matecoin_account"] -= amount
 
 
-def creat_buy(amount: decimal, coin_price: decimal) -> None:
+def creat_buy(amount: decimal, coin_price: decimal, profit: dict) -> None:
     profit["earned_money"] -= amount * coin_price
     profit["matecoin_account"] += amount
 
 
-def create_transactions(actions: str) -> None:
+def create_transactions(actions: str, profit: dict) -> None:
     for action in actions:
-        create_one_transaction(action)
+        create_one_transaction(action, profit)
 
 
-def create_one_transaction(action: str) -> None:
+def create_one_transaction(action: str, profit: dict) -> None:
     if action["bought"] is not None:
-        creat_buy(action["bought"], action["matecoin_price"])
+        creat_buy(action["bought"], action["matecoin_price"], profit)
 
     if action["sold"] is not None:
-        creat_sold(action["sold"], action["matecoin_price"])
+        creat_sold(action["sold"], action["matecoin_price"], profit)
 
 
 def prepare_js_file_after_read(js_file: str) -> str:
@@ -57,15 +52,24 @@ def float_to_decimal(value: str) -> decimal:
     return decimal.Decimal(value)
 
 
-def read_js_file(file_name: str) -> str:
-    with open(file_name, "r") as file:
-        mentors = json.load(file)
-        return mentors
+# def read_js_file(file_name: str) -> str:
+#     with open(file_name, "r") as file:
+#         mentors = json.load(file)
+#         return mentors
 
 
 def calculate_profit(file_name: str) -> None:
-    js_file_as_list = read_js_file(file_name)
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    print(dir_path)
+    profit = {
+        "earned_money": decimal.Decimal(0),
+        "matecoin_account": decimal.Decimal(0)
+    }
+
+    with open(file_name, "r") as file:
+        js_file_as_list = json.load(file)
+
     js_str_as_decimal = prepare_js_file_after_read(js_file_as_list)
-    create_transactions(js_str_as_decimal)
+    create_transactions(js_str_as_decimal, profit)
     js_decimal_as_str = prepare_decimal_dict_for_dump(profit)
-    write_js_file("profit.json", js_decimal_as_str)
+    write_js_file(file_name, js_decimal_as_str)
