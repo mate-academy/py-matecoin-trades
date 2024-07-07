a636cbaf9e130203ba479e7f37fce31d4605c5d2
MRGL <Sonemongoo@gmail.com>
2024-07-07 20:59:42 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,30 +1,31 @@
-import json
 from decimal import Decimal
+import json
 
 
 def calculate_profit(file_name: str) -> None:
-    with open(file_name, "r") as f:
-        trades = json.load(f)
+    data_file = []
+    with open(file_name, "r", encoding="utf-8") as file:
+        data_file = json.load(file)
 
-    earned_money = Decimal("0")
-    matecoin_account = Decimal("0")
-    for trade in trades:
-        bought = Decimal(trade["bought"]) if trade["bought"] else Decimal("0")
-        sold = Decimal(trade["sold"]) if trade["sold"] else Decimal("0")
-        matecoin_price = Decimal(trade["matecoin_price"])
+    earned_money = Decimal(value="0")
+    matecoin_account = Decimal(value=0)
+    for item in data_file:
+        if item["bought"]:
+            bought_volume = Decimal(item["bought"])
+            bought_price = Decimal(item["matecoin_price"])
+            cost = bought_volume * bought_price
+            matecoin_account += bought_volume
+            earned_money -= cost
+        if item["sold"]:
+            sold_volume = Decimal(item["sold"])
+            sold_price = Decimal(item["matecoin_price"])
+            income = sold_volume * sold_price
+            matecoin_account -= sold_volume
+            earned_money += income
 
-        matecoin_account += bought
-        matecoin_account -= sold
-        earned_money -= bought * matecoin_price
-        earned_money += sold * matecoin_price
     result = {
         "earned_money": str(earned_money),
         "matecoin_account": str(matecoin_account)
     }
-
-    with open("profit.json", "w") as file:
+    with open("profit.json", "w", encoding="utf-8") as file:
         json.dump(result, file, indent=2)
-
-
-if __name__ == "__main__":
-    calculate_profit("trades.json")
