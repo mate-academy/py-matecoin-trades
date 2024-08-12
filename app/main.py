39ae597c4da0db24013df2d6af46333448949e9e
serhii <ockar2018@gmail.com>
2024-08-12 19:18:37 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,31 +1,31 @@
 import json
-import os
 from decimal import Decimal
 
 
 def calculate_profit(trades_info: str) -> None:
-    filepath = os.path.join(os.path.dirname(__file__), trades_info)
-    with open(filepath, "r") as file:
+    with open(trades_info, "r") as file:
         trades = json.load(file)
 
-    earned_money = Decimal("0")
-    matecoin_account = Decimal("0")
-
-    for trade in trades:
-        if trade["bought"]:
-            matecoin_account += Decimal(trade["bought"])
-            earned_money -= (Decimal(trade["matecoin_price"])
-                             * Decimal(trade["bought"]))
-
-        if trade["sold"]:
-            matecoin_account -= Decimal(trade["sold"])
-            earned_money += (Decimal(trade["matecoin_price"])
-                             * Decimal(trade["sold"]))
-
-    result = {
-        "earned_money": str(earned_money),
-        "matecoin_account": str(matecoin_account)
+    profit_dict = {
+        "earned_money": Decimal("0"),
+        "matecoin_account": Decimal("0")
     }
 
-    with open("profit.json", "w") as f:
-        json.dump(result, f)
+    for trade in trades:
+        if trade["bought"] is not None:
+            profit_dict["earned_money"] -= (Decimal(trade["bought"])
+                                            * Decimal(trade["matecoin_price"]))
+            profit_dict["matecoin_account"] += Decimal(trade["bought"])
+
+        if trade["sold"] is not None:
+            profit_dict["earned_money"] += (Decimal(trade["sold"])
+                                            * Decimal(trade["matecoin_price"]))
+            profit_dict["matecoin_account"] -= Decimal(trade["sold"])
+
+    profit_dict = {
+        "earned_money": str(profit_dict["earned_money"]),
+        "matecoin_account": str(profit_dict["matecoin_account"])
+    }
+
+    with open("profit.json", "w") as file:
+        json.dump(profit_dict, file, indent=2)
