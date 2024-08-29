581fab6cfcf6c126022e8a87757cbd55bd4f41d0
Ekaterina <sudarkova.ekaterina@gmail.com>
2024-08-29 21:52:06 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 import json
-import os
 from decimal import Decimal
 
 
-def calculate_profit(file_name: json) -> None:
+def calculate_profit(file_name: str) -> None:
     with open(file_name, "r") as file:
         trades = json.load(file)
     earned_money = Decimal("0.0")
@@ -24,8 +23,5 @@ def calculate_profit(file_name: json) -> None:
         "earned_money": str(earned_money),
         "matecoin_account": str(matecoin_account)
     }
-    file_path = os.path.join(
-        "/Users/macbookair/environment_setup/py-matecoin-trades", "profit.json"
-    )
-    with open(file_path, "w") as file:
+    with open("profit.json", "w") as file:
         json.dump(result, file, indent=2)
