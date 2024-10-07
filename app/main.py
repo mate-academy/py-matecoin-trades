15baee561e142a35a9cca122d0a4205ad2c1d1ef
Lev <lev2099@gmail.com>
2024-10-07 14:38:26 +0300

    add calculate_profit

--- app/main.py
+++ app/main.py
@@ -23,5 +23,8 @@ def calculate_profit(name_file: str) -> None:
         "matecoin_account": str(matecoin_account)
     }
 
-    with open("../profit.json", "w") as file:
+    with open("profit.json", "w") as file:
         json.dump(profit, file, indent=2)
+
+
+calculate_profit("trades.json")
