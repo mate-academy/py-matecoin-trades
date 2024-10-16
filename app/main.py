c1fa3484d3b551a22887b8a30c30472bfbb7cccb
Fibonacci <volokitin.vitaliy@icloud.com>
2024-10-16 19:45:50 +0300

    complated

--- app/main.py
+++ app/main.py
@@ -1,1 +1,34 @@
-# write your code here
+import json
+
+from decimal import Decimal
+
+from typing import Union
+
+
+def calculate_profit(trades_file: str) -> Union[None, dict]:
+
+    with open(trades_file, "r") as file:
+        trades = json.load(file)
+
+    earned_money = Decimal("0")
+    matecoin_account = Decimal("0")
+
+    for trade in trades:
+        if trade["bought"]:
+            bought_volume = Decimal(trade["bought"])
+            matecoin_price = Decimal(trade["matecoin_price"])
+            matecoin_account += bought_volume
+            earned_money -= bought_volume * matecoin_price
+        if trade["sold"]:
+            sold_volume = Decimal(trade["sold"])
+            matecoin_price = Decimal(trade["matecoin_price"])
+            matecoin_account -= sold_volume
+            earned_money += sold_volume * matecoin_price
+
+    result = {
+        "earned_money": str(earned_money),
+        "matecoin_account": str(matecoin_account)
+    }
+
+    with open("profit.json", "w") as file:
+        json.dump(result, file, indent=4)
