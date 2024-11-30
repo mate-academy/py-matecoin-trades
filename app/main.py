5e6b321091f9b5e98da32e66adb3334bee5b9511
Ilia Terentev <wenhun1@gmail.com>
2024-11-30 11:40:22 +0200

    Solution with 1 mistake without flake8

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import json
 
 import decimal
 
+
 def calculate_profit(file_name: str) -> None:
     with open(file_name, "r") as file:
         transaction_data = json.load(file)
@@ -13,11 +14,14 @@ def calculate_profit(file_name: str) -> None:
     for data in transaction_data:
         if data["bought"]:
             matecoin_account += decimal.Decimal(data["bought"])
-            bought_total += decimal.Decimal(data["bought"]) * decimal.Decimal(data["matecoin_price"])
+            bought_total += (decimal.Decimal(data["bought"])
+                             * decimal.Decimal(data["matecoin_price"]))
         if data["sold"]:
             matecoin_account -= decimal.Decimal(data["sold"])
-            sold_total += decimal.Decimal(data["sold"]) * decimal.Decimal(data["matecoin_price"])
+            sold_total += (decimal.Decimal(data["sold"])
+                           * decimal.Decimal(data["matecoin_price"]))
 
     with open("profit.json", "w") as profit_file:
-        json.dump({"earned_money": str(sold_total - bought_total), "matecoin_account": str(matecoin_account)},
+        json.dump({"earned_money": str(sold_total - bought_total),
+                   "matecoin_account": str(matecoin_account)},
                   profit_file, indent=2)
