861bf990192ed4ffe3d1cb05a9185313e7419f94
IvankaKuzin <kuziniwanka@gmail.com>
2024-12-20 00:01:17 +0200

    Changed counting2

--- app/main.py
+++ app/main.py
@@ -6,33 +6,26 @@ def calculate_profit(trades_file: str) -> None:
     with open(trades_file, "r") as file:
         trades = json.load(file)
 
-    sold_count_all_in_dollars = sum(
-        [
-            Decimal(trade["sold"]) * Decimal(trade["matecoin_price"])
-            for trade in trades
-            if trade["bought"] is None
-        ]
-    )
-    bought_count_all_in_dollars = sum(
-        [
-            Decimal(trade["bought"]) * Decimal(trade["matecoin_price"])
-            for trade in trades
-            if trade["sold"] is None
-        ]
-    )
+    sold_count_all_in_dollars = 0
+    sold_count = 0
+    bought_count_all_in_dollars = 0
+    bought_count = 0
 
-    sold_count = sum(
-        [Decimal(trade["sold"]) for trade in trades if trade["bought"] is None]
-    )
-    bought_count = sum(
-        [Decimal(trade["bought"]) for trade in trades if trade["sold"] is None]
-    )
+    for trade in trades:
+        if trade["bought"] is None:
+            sold_count_all_in_dollars += Decimal(trade["sold"]) * Decimal(trade["matecoin_price"])
+            sold_count += Decimal(trade["sold"])
+        if trade["sold"] is None:
+            bought_count_all_in_dollars += Decimal(trade["bought"]) * Decimal(trade["matecoin_price"])
+            bought_count += Decimal(trade["bought"])
 
     result_dict = {
-        "earned_money": str(bought_count_all_in_dollars
-                            - sold_count_all_in_dollars),
+        "earned_money": str(sold_count_all_in_dollars
+                        - bought_count_all_in_dollars),
         "matecoin_account": str(sold_count - bought_count),
     }
 
     with open("profit.json", "w") as file:
-        json.dump(result_dict, file)
+        json.dump(result_dict, file, indent=2)
+
+# calculate_profit("trades.json")
