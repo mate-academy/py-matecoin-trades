6987b57b0bdb5266ff22571c179eb2dcacca4650
IvankaKuzin <kuziniwanka@gmail.com>
2024-12-20 16:33:05 +0200

    Changed code style

--- app/main.py
+++ app/main.py
@@ -13,21 +13,20 @@ def calculate_profit(trades_file: str) -> None:
 
     for trade in trades:
         if trade["bought"] is None:
-            sold_count_all_in_dollars += (Decimal(trade["sold"])
-                                          * Decimal(trade["matecoin_price"]))
+            sold_count_all_in_dollars += Decimal(trade["sold"]) * Decimal(
+                trade["matecoin_price"]
+            )
             sold_count += Decimal(trade["sold"])
         if trade["sold"] is None:
-            bought_count_all_in_dollars += (Decimal(trade["bought"])
-                                            * Decimal(trade["matecoin_price"]))
+            bought_count_all_in_dollars += Decimal(trade["bought"]) * Decimal(
+                trade["matecoin_price"]
+            )
             bought_count += Decimal(trade["bought"])
 
     result_dict = {
-        "earned_money": str(sold_count_all_in_dollars
-                        - bought_count_all_in_dollars),
-        "matecoin_account": str(sold_count - bought_count),
+            "earned_money": str(sold_count_all_in_dollars - bought_count_all_in_dollars),
+            "matecoin_account": str(sold_count - bought_count),
     }
 
     with open("profit.json", "w") as file:
         json.dump(result_dict, file, indent=2)
-
-# calculate_profit("trades.json")
