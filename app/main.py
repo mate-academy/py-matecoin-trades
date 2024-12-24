c36c3cd41d4fc57cc864dce833ab91f256b12d42
Andrii Ovod <ovodenok@mail.com>
2024-12-24 12:54:02 +0200

    Solution after verification flake8

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 import json
 from decimal import Decimal
 
@@ -6,10 +7,19 @@ def calculate_profit(trades_file: str) -> None:
     earned_money = Decimal(0)
     matecoin_account = Decimal(0)
 
-    with open("/Users/admin/environment_setup/py-matecoin-trades/app/"
-              "trades.json", "r") as file:
-        trades = json.load(file)
+    # Отримуємо шлях до поточного скрипта та будуємо шлях до файлу
+    file_dir = os.path.dirname(os.path.realpath(__file__))
+    file_path = os.path.join(file_dir, trades_file)
 
+    # Перевірка наявності файлу
+    if os.path.exists(file_path):
+        with open(file_path, "r") as file:
+            trades = json.load(file)
+    else:
+        print(f"File {file_path} not found!")
+        return
+
+    # Обробка даних з файлу
     for trade in trades:
         matecoin_price = Decimal(trade["matecoin_price"])
         if trade["bought"] is not None:
@@ -21,11 +31,13 @@ def calculate_profit(trades_file: str) -> None:
             matecoin_account -= sold
             earned_money += sold * matecoin_price
 
+    # Підготовка результату
     result = {
         "earned_money": str(earned_money),
         "matecoin_account": str(matecoin_account),
     }
 
+    # Запис результату у файл profit.json
     with open("profit.json", "w") as output_file:
         json.dump(result, output_file, indent=2)
 
