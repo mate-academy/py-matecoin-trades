9bf1eccece7c1946c1cc8525327f1a4bedac0776
neeeksa <avgust.k31@gmail.com>
2025-01-26 02:01:33 +0200

    first_try

--- app/main.py
+++ app/main.py
@@ -1,1 +1,35 @@
-# write your code here
+import json
+from decimal import Decimal, ROUND_HALF_UP
+
+
+def calculate_profit(file_name: str) -> None:
+    with open(file_name, "r") as trade_file:
+        trades = json.load(trade_file)
+
+    earned_money = Decimal(0)
+    matecoin_account = Decimal(0)
+
+    for trade in trades:
+
+        if trade.get("bought"):
+            matecoin_account += Decimal(trade["bought"])
+
+        if trade.get("sold"):
+            earned_money += \
+                (
+                    Decimal(trade["sold"]) * Decimal(trade["matecoin_price"])
+                )
+
+    earned_money = (
+        earned_money.quantize(Decimal("0.00000001"), rounding=ROUND_HALF_UP))
+    matecoin_account = (
+        matecoin_account.quantize(
+            Decimal("0.00000001"), rounding=ROUND_HALF_UP))
+
+    result = {
+        "earned_money": f"{earned_money: .8f}",
+        "matecoin_account": f"{matecoin_account: .8f}"
+    }
+
+    with open("profit.json", "w") as profit_file:
+        json.dump(result, profit_file, indent=4)
