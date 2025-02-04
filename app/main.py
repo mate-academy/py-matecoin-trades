e746a6f51e1a6090132a2e3124bec882dd9b3ad6
Tymofii Khlistik <tima0323199@gmail.com>
2025-02-04 23:21:43 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -6,8 +6,8 @@ def calculate_profit(file_name: str) -> None:
     with open(file_name, "r") as file:
         trades = json.load(file)
 
-    earned_money = Decimal("0.0")
-    matecoin_account = Decimal("0.0")
+    earned_money = Decimal("0")
+    matecoin_account = Decimal("0")
 
     for trade in trades:
         if trade["bought"]:
@@ -28,7 +28,7 @@ def calculate_profit(file_name: str) -> None:
     }
 
     with open("profit.json", "w") as file:
-        json.dump(result, file, indent=4)
+        json.dump(result, file, indent=2)
 
 
 if __name__ == "__main__":
