d359ed98b0d4e29a5aefb5ed655f6abc2e5f1897
Ksenia Malashchuk <malashchuk.ksenia@gmail.com>
2025-02-05 23:12:36 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -28,4 +28,4 @@ def calculate_profit(filename: str) -> None:
     }
 
     with open("profit.json", "w") as file:
-        json.dump(profit_data, file, indent=4)
+        json.dump(profit_data, file, indent=2)
