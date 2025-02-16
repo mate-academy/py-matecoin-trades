ddf0d38d2fb263b457ea3c5f90642a5ddcef2b79
Anton <anton-flash@ukr.net>
2025-02-16 22:22:58 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,16 +11,16 @@ def calculate_profit(file_name: str) -> None:
         if trade["bought"]:
             bought = Decimal(trade["bought"])
             profit["matecoin_account"] += bought
-            profit["earned_money"] -= bought * Decimal(trade["matecoin_price"]
+            profit["earned_money"] -= bought * Decimal(trade["matecoin_price"])
         if trade["sold"]:
             sold = Decimal(trade["sold"])
-            profit["earned_money"] -= sold
-            profit["earned_money"] += sold * Decimal(trade["matecoin_price"]
-            
+            profit["matecoin_account"] -= sold
+            profit["earned_money"] += sold * Decimal(trade["matecoin_price"])
+
     result = {
         "earned_money": str(profit["earned_money"]),
         "matecoin_account": str(profit["matecoin_account"])
     }
-    
+
     with open("profit.json", "w") as f:
         json.dump(result, f, indent=2)
