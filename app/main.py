d0d8c9ecc08041440bebe616312d9d7a0978c206
Andrii <yurkivandriy02@gmail.com>
2025-03-04 19:36:24 +0200

    Completed task mate-coin

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+import json
+from decimal import Decimal
+
+
+def calculate_profit(file_name: str):
+    with open(file_name, "r") as file:
+        traders_data = json.load(file)
+
+    profit = 0
+    volume = 0
+
+    for order in traders_data:
+        if order['bought']:
+            profit -= Decimal(order['bought']) * Decimal(order["matecoin_price"])
+            volume += Decimal(order['bought'])
+        if order['sold']:
+            profit += Decimal(order['sold']) * Decimal(order["matecoin_price"])
+            volume -= Decimal(order['sold'])
+
+    trading_result = {"earned_money": str(profit), "matecoin_account": str(volume)}
+
+    with open("profit.json", "w") as file:
+        json.dump(trading_result, file, indent=2)
