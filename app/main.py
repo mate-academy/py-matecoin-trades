cad670e23fc683b33107892fe584a9fd8b136314
Maksym Omelkov <omelkov85@gmail.com>
2025-03-11 14:19:56 +0200

    Matecoin trades (1.0)

--- app/main.py
+++ app/main.py
@@ -1,1 +1,27 @@
-# write your code here
+import json
+from decimal import Decimal
+
+
+def calculate_profit(file_name: str) -> None:
+    with open(file_name, "r") as file:
+        trades_list = json.load(file)
+    matecoin_account = 0
+    earned_money = 0
+
+    for trade in trades_list:
+        if trade["bought"] is not None:
+            matecoin_account += Decimal(trade["bought"])
+            earned_money -= (Decimal(trade["matecoin_price"])
+                             * Decimal(trade["bought"]))
+        if trade["sold"] is not None:
+            matecoin_account -= Decimal(trade["sold"])
+            earned_money += (Decimal(trade["matecoin_price"])
+                             * Decimal(trade["sold"]))
+
+    profit_dict = {
+        "earned_money": str(earned_money),
+        "matecoin_account": str(matecoin_account)
+    }
+
+    with open("profit.json", "w") as file:
+        json.dump(profit_dict, file)
