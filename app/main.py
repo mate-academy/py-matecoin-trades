8a960a398ef97bd457e349f16d687cc11f313630
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-05-01 15:39:11 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,9 +11,12 @@ def calculate_profit(file_name: str) -> None:
                 item[key] = 0
             else:
                 item[key] = decimal.Decimal(value)
-    earned_money = sum([trade["sold"] * trade["matecoin_price"] for trade in trade_info])
-    matecoin_account = sum([trade["bought"] for trade in trade_info]) - sum([trade["sold"] for trade in trade_info])
-    profit_dict = {"earned_money": float(earned_money), "matecoin_account": float(matecoin_account)}
+    earned_money = sum([trade["sold"] * trade["matecoin_price"] 
+                        for trade in trade_info])
+    matecoin_account = (sum([trade["bought"] for trade in trade_info])
+                        - sum([trade["sold"] for trade in trade_info]))
+    profit_dict = {"earned_money": float(earned_money), "matecoin_account": 
+        float(matecoin_account)}
 
     with open("profit.json", "w") as file:
         json.dump(profit_dict, file)
