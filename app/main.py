3fda563dbc581964460b6ee1c5df867ae13f2cbd
Bartek Pietrzyk <bartekpietrzyk@interia.eu>
2025-05-26 20:49:24 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,33 +1,30 @@
-from decimal import Decimal
 import json
+from decimal import Decimal
 
 
-def calculate_profit(trades_path: str) -> None:
-    from pathlib import Path
-
-    trades_file = Path(trades_path)
-    with trades_file.open() as file:
+def calculate_profit(trades_file: str) -> None:
+    with open(trades_file, "r") as file:
         trades = json.load(file)
 
-    earned_money = Decimal("0")
-    matecoin_account = Decimal("0")
+    count_in_dollars = 0
+    count_matecoin = 0
 
     for trade in trades:
-        matecoin_price = Decimal(trade["matecoin_price"])
-        if trade["bought"]:
-            matecoin_account += Decimal(trade["bought"])
-            earned_money -= Decimal(trade["bought"]) * matecoin_price
-        if trade["sold"]:
-            matecoin_account -= Decimal(trade["sold"])
-            earned_money += Decimal(trade["sold"]) * matecoin_price
+        if trade["bought"] is not None:
+            count_in_dollars -= Decimal(trade["bought"]) * Decimal(
+                trade["matecoin_price"]
+            )
+            count_matecoin += Decimal(trade["bought"])
+        if trade["sold"] is not None:
+            count_in_dollars += Decimal(trade["sold"]) * Decimal(
+                trade["matecoin_price"]
+            )
+            count_matecoin -= Decimal(trade["sold"])
 
-    result = {
-        "earned_money" : f"{earned_money: .7f}",
-        "matecoin_account" : f"{matecoin_account: .5f}",
+    result_dict = {
+        "earned_money": str(count_in_dollars),
+        "matecoin_account": str(count_matecoin),
     }
 
-    profit_file = trades_file.parent / "profit.json"
-    with profit_file.open("w") as file:
-        json.dump(result, file, indent=2)
-
-    return None
+    with open("profit.json", "w") as file:
+        json.dump(result_dict, file, indent=2)
