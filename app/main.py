a41552462bc66ddfdfc01dbfcf1d2f4e88c6b4cb
Bartek Pietrzyk <bartekpietrzyk@interia.eu>
2025-05-26 20:12:42 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,41 +2,38 @@ import json
 from decimal import Decimal
 
 
-def calculate_profit(file_name: str = "trades.json" ):
+def calculate_profit(file_name: str = "trades.json") -> None:
 
-    data = json.load(open("trades.json"))
-    data_bought =[]
+    data = json.load(open(file_name))
+    data_bought = []
     data_sold = []
     account_bought = []
     account_sold = []
-    new_data = []
 
     for i in data:
-        if i["bought"] == None:
+        if i["bought"] is None:
             continue
         else:
-            price_bought = Decimal(float(i["bought"])) * Decimal(float(i["matecoin_price"]))
+            price_bought = (Decimal(i["bought"])
+                            * Decimal(i["matecoin_price"]))
             data_bought.append(price_bought)
             account_bought.append(float(i["bought"]))
+
     for i in data:
-        if i["sold"] == None:
+        if i["sold"] is None:
             continue
         else:
-            price_sold = Decimal(float(i["sold"])) * Decimal(float(i["matecoin_price"]))
+            price_sold = (Decimal(i["sold"])
+                          * Decimal(i["matecoin_price"]))
             data_sold.append(price_sold)
             account_sold.append(float(i["sold"]))
-
 
     matecoin_account = (sum(account_bought) - sum(account_sold))
     earned_money = (sum(data_bought) - sum(data_sold))
 
     new_data = {
-    "earned_money": str(earned_money),
-    "matecoin_account": str(matecoin_account)}
-
+        "earned_money": str(earned_money),
+        "matecoin_account": str(matecoin_account)}
 
     with open("profit.json", "w") as file:
-        mentors = json.dump(new_data, file)
-
-
-calculate_profit()
+        json.dump(new_data, file)
