44a1c35718be8d7b068fb2044b0ab9d6eed6a2a0
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-07-15 11:18:27 +0300

    fixed all I hope

--- app/main.py
+++ app/main.py
@@ -1,28 +1,33 @@
 import json
-from decimal import Decimal as Dec
+from decimal import Decimal
 
 
 def calculate_profit(filename: str) -> None:
-    purchase, sale, coin_count = Dec("0"), Dec("0"), Dec("0")
+    purchase, sale, coin_count = (
+        Decimal("0"),
+        Decimal("0"),
+        Decimal("0")
+    )
 
     with open(filename, "r") as json_trades:
         trades = json.load(json_trades)
 
         for trade in trades:
+            price = Decimal(trade["matecoin_price"])
+
             if trade.get("bought") is not None:
-                purchase += Dec(trade["bought"]) * Dec(trade["matecoin_price"])
-                coin_count += Dec(trade["bought"])
+                bought_amount = Decimal(trade["bought"])
+                purchase += bought_amount * price
+                coin_count += bought_amount
             if trade.get("sold") is not None:
-                sale += Dec(trade["sold"]) * Dec(trade["matecoin_price"])
-                coin_count -= Dec(trade["sold"])
+                sold_amount = Decimal(trade["sold"])
+                sale += sold_amount * price
+                coin_count -= sold_amount
 
     with open("../profit.json", "w") as json_profit:
         profit = sale - purchase
-        json.dump(
-            {
-                "earned_money": str(profit),
-                "matecoin_account": str(coin_count)
-            },
-            json_profit,
-            indent=2
-        )
+        json_result = {
+            "earned_money": str(profit),
+            "matecoin_account": str(coin_count)
+        }
+        json.dump(json_result, json_profit, indent=2)
