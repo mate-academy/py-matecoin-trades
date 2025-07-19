70b976335183b04ebac440a56e30439e8a87d4ba
Vlad <vladlubeka147@gmail.com>
2025-07-19 15:38:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,26 +1,27 @@
 import json
 from decimal import Decimal, getcontext
+from typing import Any
 
 
-def calculate_profit(filename):
+def calculate_profit(filename: str) -> None:
     getcontext().prec = 28
 
-    with open(filename, 'r') as f:
-        trades = json.load(f)
+    with open(filename, "r") as f:
+        trades: list[dict[str, Any]] = json.load(f)
 
-    earned_money = Decimal('0')
-    matecoin_account = Decimal('0')
+    earned_money = Decimal("0")
+    matecoin_account = Decimal("0")
 
     for trade in trades:
-        price = Decimal(trade['matecoin_price'])
+        price = Decimal(trade["matecoin_price"])
 
-        if trade['bought']:
-            bought = Decimal(trade['bought'])
+        if trade["bought"]:
+            bought = Decimal(trade["bought"])
             earned_money -= bought * price
             matecoin_account += bought
 
-        if trade['sold']:
-            sold = Decimal(trade['sold'])
+        if trade["sold"]:
+            sold = Decimal(trade["sold"])
             earned_money += sold * price
             matecoin_account -= sold
 
