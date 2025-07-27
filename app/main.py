28b080cb45556f06e8de3b6931fe2a1c9e92d7e1
PEMOHTOKOH <andrii.serdiukov@nure.ua>
2025-07-28 01:24:40 +0300

    solution2

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 PROFIT = f"{BASE_DIR}/profit.json"
 
 
-def calculate_profit(trades):
+def calculate_profit(trades: str) -> None:
     with open(trades) as json_data:
         data = json.load(json_data)
 
@@ -16,24 +16,23 @@ def calculate_profit(trades):
     for item in data:
         spend = 0
         received = 0
-        price = decimal.Decimal(item['matecoin_price'])
-        if item['bought'] is not None:
-            bought = decimal.Decimal(item['bought'])
+        price = decimal.Decimal(item["matecoin_price"])
+        if item["bought"] is not None:
+            bought = decimal.Decimal(item["bought"])
             matecoins += bought
             spend = bought * price
-        if item['sold'] is not None:
-            sold = decimal.Decimal(item['sold'])
+        if item["sold"] is not None:
+            sold = decimal.Decimal(item["sold"])
             matecoins -= sold
             received = sold * price
         profit += received - spend
 
-    with open(PROFIT, 'w') as f:
+    with open(PROFIT, "w") as f:
         json.dump(
             {
-                'earned_money': str(profit),
-                'matecoin_account': str(matecoins)
-                },
-            f,
-            indent=2,
+                "earned_money": str(profit),
+                "matecoin_account": str(matecoins)
+            },
+            f, indent=2
         )
     return None
