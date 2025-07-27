875bc5934f47f9f67b6c764bb2cbf8695940b3b7
PEMOHTOKOH <andrii.serdiukov@nure.ua>
2025-07-28 01:15:37 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,39 @@
-# write your code here
+import json
+import decimal
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+PROFIT = f"{BASE_DIR}/profit.json"
+
+
+def calculate_profit(trades):
+    with open(trades) as json_data:
+        data = json.load(json_data)
+
+    profit = 0
+    matecoins = 0
+
+    for item in data:
+        spend = 0
+        received = 0
+        price = decimal.Decimal(item['matecoin_price'])
+        if item['bought'] is not None:
+            bought = decimal.Decimal(item['bought'])
+            matecoins += bought
+            spend = bought * price
+        if item['sold'] is not None:
+            sold = decimal.Decimal(item['sold'])
+            matecoins -= sold
+            received = sold * price
+        profit += received - spend
+
+    with open(PROFIT, 'w') as f:
+        json.dump(
+            {
+                'earned_money': str(profit),
+                'matecoin_account': str(matecoins)
+                },
+            f,
+            indent=2,
+        )
+    return None
