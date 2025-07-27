462695ea8c218642916193cb774eb86db7a22b04
Anna Marchenko <marchenkoanna0464@gmail.com>
2025-07-28 00:56:55 +0200

    dk

--- app/main.py
+++ app/main.py
@@ -2,11 +2,9 @@ import json
 from decimal import Decimal
 
 
-def calculate_profit(document) -> None:
+def calculate_profit(document: str) -> None:
     with open(document, "r") as file:
         currency = json.load(file)
-
-
 
     result = {
         "matecoin_account": Decimal("0"),
@@ -17,20 +15,19 @@ def calculate_profit(document) -> None:
         price = Decimal(entry["matecoin_price"])
 
         if entry["bought"] is not None:
-            bought_amount = Decimal(entry["bought"])
-            result["matecoin_account"] += bought_amount
-            result["earned_money"] -= bought_amount * price
+            result["matecoin_account"] += Decimal(entry["bought"])
+            result["earned_money"] -= (Decimal(entry["bought"]) * price)
 
         if entry["sold"] is not None:
-            sold_amount = Decimal(entry["sold"])
-            result["matecoin_account"] -= sold_amount
-            result["earned_money"] += sold_amount * price
+            result["matecoin_account"] -= Decimal(entry["sold"])
+            result["earned_money"] += (Decimal(entry["sold"]) * price)
 
     with open("profit.json", "w") as result_file:
         json.dump(
             {
+                "earned_money": str(result["earned_money"]),
                 "matecoin_account": str(result["matecoin_account"]),
-                "earned_money": str(result["earned_money"])
+
             },
-            result_file
-        )
+            result_file,
+            indent=2)
