890afb4e2772ff7e2a1ad1ab23498c0a5e379fe2
Valentyn Pyzhyk <pyzhyk.business@gmail.com>
2025-07-30 00:23:17 +0300

    Changed in order to meet all flake8 requirements

--- app/main.py
+++ app/main.py
@@ -1,23 +1,24 @@
 import json
 from decimal import Decimal
 
-def calculate_profit(filename):
-    with open(filename, 'r') as f:
+
+def calculate_profit(filename: str) -> None:
+    with open(filename, "r") as f:
         trades = json.load(f)
 
-    earned_money = Decimal('0')
-    matecoin_account = Decimal('0')
+    earned_money = Decimal("0")
+    matecoin_account = Decimal("0")
 
     for trade in trades:
-        price = Decimal(trade['matecoin_price'])
+        price = Decimal(trade["matecoin_price"])
 
-        if trade['bought']:
-            volume = Decimal(trade['bought'])
+        if trade["bought"]:
+            volume = Decimal(trade["bought"])
             matecoin_account += volume
             earned_money -= volume * price
 
-        if trade['sold']:
-            volume = Decimal(trade['sold'])
+        if trade["sold"]:
+            volume = Decimal(trade["sold"])
             matecoin_account -= volume
             earned_money += volume * price
 
