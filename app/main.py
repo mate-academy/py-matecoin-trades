dee29e805456d7d58aaa68acd21919f6144eeb40
0lex1y <kotov.oleksiy@icloud.com>
2025-09-22 02:39:47 +0300

    test script

--- app/main.py
+++ app/main.py
@@ -2,23 +2,30 @@ import json
 from decimal import Decimal
 
 
-def calculate_profit(trades_file: list) -> None:
-    earned_money = Decimal("0")
-    matecoin_account = Decimal("0")
+def calculate_profit(trades_file: str) -> None:
     with open(trades_file, "r") as json_file:
-        data = json.load(json_file)
-        for trade in data:
-            bought = Decimal(trade["bought"]) if trade["bought"] else None
-            sold = Decimal(trade["sold"]) if trade["sold"] else None
-            price = Decimal(trade["matecoin_price"])
+        transaction = json.load(json_file)
+    matecoin_account = Decimal("0")
+    spent = Decimal("0")
+    received = Decimal("0")
 
-            if bought:
-                matecoin_account += bought
-            if sold:
-                earned_money += sold * price
-                matecoin_account -= sold
+    for trade in transaction:
+        price = Decimal(trade["matecoin_price"])
+        if trade["bought"] is not None:
+            bought = Decimal(trade["bought"])
+            spent += bought * price
+            matecoin_account += bought
+        if trade["sold"] is not None:
+            sold = Decimal(trade["sold"])
+            received += sold * price
+            matecoin_account -= sold
+
+    earned_money = received - spent
+
+    report = {
+        "earned_money": str(earned_money),
+        "matecoin_account": str(matecoin_account)
+    }
 
     with open("profit.json", "w") as profit_file:
-        json.dump({
-            "earned_money": str(earned_money),
-            "matecoin_account": str(matecoin_account)}, profit_file, indent=5)
+        json.dump(report, profit_file, indent=2)
