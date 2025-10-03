899db17d5dda73b25a63a9a23453e3ec7ff04064
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2025-10-02 23:20:53 -0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,21 +1,27 @@
 import json
 from decimal import Decimal
-import os
 
 
 def calculate_profit(file_name: str) -> None:
-    data_account = {"earned_money": Decimal("0.00"), "matecoin_account": Decimal("0.00")}
-    
+    data_account = {
+        "earned_money": Decimal("0.00"),
+        "matecoin_account": Decimal("0.00")
+    }
+
     with open(file_name, "r") as file:
         transactions = json.load(file)
 
     for transaction in transactions:
+        matecoin_price = Decimal(transaction["matecoin_price"])
+
         if transaction["bought"] is not None:
-            data_account["matecoin_account"] += Decimal(transaction["bought"])
-            data_account["earned_money"] -= Decimal(transaction["bought"]) * Decimal(transaction["matecoin_price"])
+            bought = Decimal(transaction["bought"])
+            data_account["matecoin_account"] += bought
+            data_account["earned_money"] -= bought * matecoin_price
         if transaction["sold"] is not None:
-            data_account["matecoin_account"] -= Decimal(transaction["sold"])
-            data_account["earned_money"] += Decimal(transaction["sold"]) * Decimal(transaction["matecoin_price"])
+            sold = Decimal(transaction["sold"])
+            data_account["matecoin_account"] -= sold
+            data_account["earned_money"] += sold * matecoin_price
 
     data_account["earned_money"] = str(data_account["earned_money"])
     data_account["matecoin_account"] = str(data_account["matecoin_account"])
