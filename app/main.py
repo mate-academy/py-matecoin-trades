046d20daa6f72fb2c342c9c486843a131616316d
Maksym Dazidov <maksymdazidov@gmail.com>
2025-10-22 19:23:44 +0200

    profit 2

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 import json
 import decimal
+from typing import Any
 
 
-def calculate_profit(file_name) -> None:
+def calculate_profit(file_name: Any) -> None:
     bought_in_dollars = 0
     sold_in_dollars = 0
     amount = 0
@@ -11,13 +12,13 @@ def calculate_profit(file_name) -> None:
     for dict_of_trade in trade_descriptions:
         if dict_of_trade["bought"]:
             bought_in_dollars\
-                += (decimal.Decimal(f"{dict_of_trade['bought']}") *
-                    decimal.Decimal(f"{dict_of_trade['matecoin_price']}"))
+                += (decimal.Decimal(f"{dict_of_trade['bought']}")
+                    * decimal.Decimal(f"{dict_of_trade['matecoin_price']}"))
             amount += decimal.Decimal(f"{dict_of_trade['bought']}")
         if dict_of_trade["sold"]:
             sold_in_dollars +=\
-                (decimal.Decimal(f"{dict_of_trade['sold']}") *
-                 decimal.Decimal(f"{dict_of_trade['matecoin_price']}"))
+                (decimal.Decimal(f"{dict_of_trade['sold']}")
+                 * decimal.Decimal(f"{dict_of_trade['matecoin_price']}"))
             amount -= decimal.Decimal(f"{dict_of_trade['sold']}")
     dict_of_profit = {
         "earned_money": str(sold_in_dollars - bought_in_dollars),
