3c89eda90230ee5eb06b21534b70f2879d96dbe8
Yan Tumar <jantumar.unity@gmail.com>
2025-11-14 17:04:41 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -6,30 +6,32 @@ getcontext().prec = 28
 
 
 def calculate_profit(trades_filename: str) -> None:
-    earned_money = Decimal('0')
-    matecoin_account = Decimal('0')
+    earned_money = Decimal("0")
+    matecoin_account = Decimal("0")
 
     try:
-        with open(trades_filename, 'r', encoding='utf-8') as f:
+        with open(trades_filename, "r", encoding="utf-8") as f:
             trades: List[Dict] = json.load(f)
     except Exception:
         return
 
     for trade in trades:
-        price = Decimal(trade['matecoin_price'])
-        if trade['bought'] is not None:
-            bought_volume = Decimal(trade['bought'])
+        price = Decimal(trade["matecoin_price"])
+
+        if trade["bought"] is not None:
+            bought_volume = Decimal(trade["bought"])
 
             matecoin_account += bought_volume
             earned_money -= bought_volume * price
 
-        if trade['sold'] is not None:
-            sold_volume = Decimal(trade['sold'])
+        if trade["sold"] is not None:
+            sold_volume = Decimal(trade["sold"])
 
             matecoin_account -= sold_volume
             earned_money += sold_volume * price
-    matecoin_result = matecoin_account.quantize(Decimal('0.00001'))
-    earned_result = earned_money.quantize(Decimal('0.0000001'))
+
+    matecoin_result = matecoin_account.quantize(Decimal("0.00001"))
+    earned_result = earned_money.quantize(Decimal("0.0000001"))
 
     profit_data = {
         "earned_money": str(earned_result),
@@ -39,7 +41,7 @@ def calculate_profit(trades_filename: str) -> None:
     output_filename = "profit.json"
 
     try:
-        with open(output_filename, 'w', encoding='utf-8') as f:
+        with open(output_filename, "w", encoding="utf-8") as f:
             json.dump(profit_data, f, indent=2)
     except Exception:
         return
