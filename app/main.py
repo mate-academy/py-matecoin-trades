39b9acd43eff523cc9f4db220ad2e80c220536fb
Yevhen Dmytrenko <zdmitrenko26@gmail.com>
2025-11-16 00:57:00 +0200

    fix decimal problem

--- app/main.py
+++ app/main.py
@@ -9,15 +9,26 @@ def calculate_profit(filename: str) -> None:
     balance = {"earned_money": Decimal("0"), "matecoin_account": Decimal("0")}
 
     for trade in trades:
-        bought_in_dollars = - Decimal(trade["bought"]) * Decimal(trade["matecoin_price"])
-        sold_in_dollars = Decimal(trade["sold"]) * Decimal(trade["matecoin_price"])
-        matecoins = Decimal(trade["bought"]) - Decimal(trade["sold"])
+
+        if trade["bought"] is None:
+            decimal_bought = Decimal("0")
+        else:
+            decimal_bought = Decimal(trade["bought"])
+
+        if trade["sold"] is None:
+            decimal_sold = Decimal("0")
+        else:
+            decimal_sold = Decimal(trade["sold"])
+
+        bought_in_dollars = - decimal_bought * Decimal(trade["matecoin_price"])
+        sold_in_dollars = decimal_sold * Decimal(trade["matecoin_price"])
+        matecoins = decimal_bought - decimal_sold
 
         balance["earned_money"] += bought_in_dollars + sold_in_dollars
         balance["matecoin_account"] += matecoins
 
+    balance["earned_money"] = str(balance["earned_money"])
+    balance["matecoin_account"] = str(balance["matecoin_account"])
+
     with open("profit.json", "w") as profit_file:
-        json.dump(balance, profit_file)
-
-
-calculate_profit("trades_1.json")
+        json.dump(balance, profit_file, indent=2)
