2a5ece195dcd5d13eda1bcad51678b4329635b18
Luan <luan.valentino78@gmail.com>
2025-11-17 11:19:32 -0300

    commit

--- app/main.py
+++ app/main.py
@@ -3,8 +3,7 @@ from decimal import Decimal
 from decimal import getcontext
 getcontext().prec = 50
 
-
-def calculate_profit(trades_file = "trades.json"):
+def calculate_profit(trades_file="trades.json"):
 
     earned_money = Decimal("0")
     matecoin_account = Decimal("0")
@@ -18,10 +17,11 @@ def calculate_profit(trades_file = "trades.json"):
     except FileNotFoundError:
         print(f"Error: Arquivo não encontrado: {trades_file}")
         return
+
     except json.JSONDecodeError:
         print(f"Error: O arquivo {trades_file} não é um JSON válido.")
         return
-
+    
     for trade in trades:
         try:
             price = Decimal(trade["matecoin_price"])
@@ -42,12 +42,10 @@ def calculate_profit(trades_file = "trades.json"):
         "earned_money": str(earned_money),
         "matecoin_account": str(matecoin_account)
     }
+
     try:
         with open(output_filename, "w", encoding="utf-8") as f:
             json.dump(result_data, f, indent=2)
         print(f"Sucesso! Resultados salvos em {output_filename}")
     except IOError:
-        print(
-            "Error: Não foi possível escrever no arquivo "
-            f"{output_filename}."
-        )
+        print(f"Error: Não foi possível escrever no arquivo {output_filename}")
