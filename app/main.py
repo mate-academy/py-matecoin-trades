211601b5851e4338c77e357cf274da8a84104d97
AlmostVictor <zhovtanetskyj.victor@gmail.com>
2025-11-20 21:14:48 +0200

    fixed

--- app/main.py
+++ app/main.py
@@ -14,18 +14,20 @@ def calculate_profit(given_file: str) -> None:
 
         bought = deal.get("bought")
         if bought:
-            amount = Decimal("bought")
+            amount = Decimal(bought)
             earned -= amount * price
             account += amount
 
         sold = deal.get("sold")
         if sold:
-            amount = Decimal("sold")
+            amount = Decimal(sold)
             earned += amount * price
             account -= amount
 
-    result = {"earned_money": earned,
-              "matecoin_account": account}
+    result = {
+        "earned_money": str(earned),
+        "matecoin_account": str(account)
+    }
 
     with open("profit.json", "w") as file:
         json.dump(result, file, indent=2)
