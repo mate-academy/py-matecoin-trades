98211ea4907209fbe669dd1237a6627fa96cf8b1
Ievgen German <german.kbrza@gmail.com>
2025-12-03 23:32:19 +0200

    solution 2

--- app/main.py
+++ app/main.py
@@ -12,15 +12,15 @@ def calculate_profit(file_name: str) -> None:
     total_earned_dollars = Decimal(0)
     for dict_with_coin in dataset:
         if dict_with_coin["bought"] is not None:
-            total_bought_coins += Decimal(dict_with_coin["bought"])
-            total_spent_dollars += (
-                    Decimal(dict_with_coin["bought"]) *
-                    Decimal(dict_with_coin["matecoin_price"]))
+            bought_coin = Decimal(dict_with_coin["bought"])
+            bought_dollars = Decimal(dict_with_coin["matecoin_price"])
+            total_bought_coins += bought_coin
+            total_spent_dollars += bought_coin * bought_dollars
         if dict_with_coin["sold"] is not None:
             total_sold_coins += Decimal(dict_with_coin["sold"])
-            total_earned_dollars += (
-                    Decimal(dict_with_coin["sold"]) *
-                    Decimal(dict_with_coin["matecoin_price"]))
+            sold_coin = Decimal(dict_with_coin["sold"])
+            sold_dollars =Decimal(dict_with_coin["matecoin_price"])
+            total_earned_dollars += sold_coin * sold_dollars
     earned_money = total_earned_dollars - total_spent_dollars
     matecoin_account = total_bought_coins - total_sold_coins
     with open("profit.json", "w", newline="", encoding="utf-8") as f:
