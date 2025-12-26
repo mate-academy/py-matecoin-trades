8f95f00a78383794cf9f51efb696d66f2b4f5d3a
Mykhaylo Zhyhan <mz5157e@gre.ac.uk>
2025-12-26 15:01:28 +0000

    read json file with trades info, calculate the profit and account balance and crate new json file and writes that info in that new file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,33 @@
-# write your code here
+import json
+from decimal import Decimal
+
+
+def calculate_profit(trades_filename: str) -> None:
+    with open(trades_filename, "r") as file_name:
+        trades = json.load(file_name)
+
+    earned = Decimal("0")
+    acc_balance = Decimal("0")
+
+    for trade in trades:
+        price = Decimal(trade["matecoin_price"])
+        bought = trade.get("bought")
+        if bought is not None:
+            vol = Decimal(bought)
+            acc_balance += vol
+            earned -= vol * price
+        sold = trade.get("sold")
+        if sold is not None:
+            vol = Decimal(sold)
+            acc_balance -= vol
+            earned += vol * price
+
+    result = {
+        "earned_money": format(earned, "f"),
+        "matecoin_account": format(acc_balance, "f"),
+    }
+
+    with open("profit.json", "w") as file_name:
+        json.dump(result, file_name, indent=2)
+
+calculate_profit("trades.json")
