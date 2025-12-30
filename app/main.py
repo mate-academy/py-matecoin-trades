7f5facd7952d7a0277aaf4ba0362226e39f65519
tomech <109527747+tomech4@users.noreply.github.com>
2025-12-30 22:53:57 +0100

    fix styling issues

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from pathlib import Path, PurePath
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 def calculate_profit(json_file: str) -> None:
     earned_money = decimal.Decimal("0")
     matecoin_account = decimal.Decimal("0")
@@ -13,11 +14,13 @@ def calculate_profit(json_file: str) -> None:
     for trade in trades_info:
         if trade["bought"]:
             matecoin_account += decimal.Decimal(trade["bought"])
-            earned_money -= decimal.Decimal(trade["bought"]) * decimal.Decimal(trade["matecoin_price"])
+            earned_money -= (decimal.Decimal(trade["bought"])
+                             * decimal.Decimal(trade["matecoin_price"]))
 
         if trade["sold"]:
             matecoin_account -= decimal.Decimal(trade["sold"])
-            earned_money += decimal.Decimal(trade["sold"]) * decimal.Decimal(trade["matecoin_price"])
+            earned_money += (decimal.Decimal(trade["sold"])
+                             * decimal.Decimal(trade["matecoin_price"]))
 
     with open(PurePath.joinpath(BASE_DIR, "profit.json"), "w") as f:
         result = {
