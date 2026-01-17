a7a4c1378c3aa46dfe171ffd332bf55f2f80c969
Julien Drolet-Noel <julien.drolet.noel@gmail.com>
2026-01-17 08:43:10 -0500

    Added Decimal initialisation

--- app/main.py
+++ app/main.py
@@ -4,19 +4,19 @@ from decimal import Decimal
 
 def calculate_profit(file_name: str) -> None:
 
-    profit = 0
-    qty = 0
+    profit = Decimal()
+    qty = Decimal()
 
     with open(file=file_name, mode="r") as file:
         trades = json.load(file)
 
         for trade in trades:
             value = trade["bought"]
-            bought_units = value if value is not None else 0
+            bought_units = value if value is not None else Decimal(0)
             bought_units = Decimal(bought_units)
 
             value = trade["sold"]
-            sold_units = value if value is not None else 0
+            sold_units = value if value is not None else Decimal(0)
             sold_units = Decimal(sold_units)
 
             price = Decimal(trade["matecoin_price"])
