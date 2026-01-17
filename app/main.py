a30ab13fb6f0d705fbc33ba7c2c46703671e6ef4
lykhorovych <31163897+lykhorovych@users.noreply.github.com>
2026-01-17 17:49:59 +0200

    Solution3

--- app/main.py
+++ app/main.py
@@ -5,27 +5,23 @@ from decimal import Decimal
 def calculate_profit(filename: str) -> None:
     with open(filename) as json_file:
         data = json.load(json_file)
-        print(data)
         profit = 0
         account = 0
         for operation in data:
-            if operation.get("sold") is None:
-                profit -= Decimal(operation["bought"]) * Decimal(operation["matecoin_price"])
-                account += Decimal(operation["bought"])
-            else:
-                profit += Decimal(operation["sold"]) * Decimal(operation["matecoin_price"])
-                account -= Decimal(operation["sold"])
+            bought_items = Decimal(operation.get("bought") or "0")
+            sold_items = Decimal(operation.get("sold") or "0")
+            mate_coin_price = Decimal(operation.get("matecoin_price"))
+            account_per_day = bought_items - sold_items
+            profit_per_day = - account_per_day * mate_coin_price
+            profit += profit_per_day
+            account += account_per_day
 
     with open("profit.json", "w") as json_target:
-        json.dump({
-            "earned_money": str(profit),
-            "matecoin_account": str(account)
-        }, json_target)
+        json.dump(
+            {"earned_money": str(profit),
+             "matecoin_account": str(account)}, json_target, indent=2
+        )
 
 
 if __name__ == "__main__":
     print(calculate_profit("app/trades.json"))
-
-
-
-
