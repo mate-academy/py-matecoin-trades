135a380e1858498d89e7762d1037438be51465d5
viki <v.yavdoschuk@gmail.com>
2026-01-24 17:15:56 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -4,8 +4,12 @@ from decimal import Decimal
 
 def calculate_profit(file_name: str) -> None:
 
-    with open("trades.json", "r") as file:
-        trades = json.load(file)
+    try:
+        with open(file_name, "r") as file:
+            trades = json.load(file)
+    except FileNotFoundError as f:
+        print("File was not found", f)
+        return
 
     profit = {"earned_money": Decimal("0"), "matecoin_account": Decimal("0")}
 
@@ -14,12 +18,12 @@ def calculate_profit(file_name: str) -> None:
             profit["matecoin_account"] += Decimal(trade["bought"])
             profit["earned_money"] -= (Decimal(trade["bought"])
                                        * Decimal(trade["matecoin_price"]))
-        elif trade["sold"] is not None:
+        if trade["sold"] is not None:
             profit["matecoin_account"] -= Decimal(trade["sold"])
-            profit["earned_money"] -= (Decimal(trade["sold"])
+            profit["earned_money"] += (Decimal(trade["sold"])
                                        * Decimal(trade["matecoin_price"]))
 
     profit_str = {key: str(value) for key, value in profit.items()}
 
     with open("profit.json", "w") as file:
-        profit = json.dump(profit_str, file)
+        json.dump(profit_str, file, indent=2)
