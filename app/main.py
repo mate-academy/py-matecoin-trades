72d60a207ac86d7c79882796c4b6e2c58ff38166
Kyryl <sautakiki.com@gmail.com>
2026-02-12 20:47:20 +0200

    sol

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import json
+from decimal import Decimal
+
+
+result = {"earned_money": Decimal("0"), "matecoin_account": Decimal("0")}
+with open("trades.json", "r") as f:
+    wallet = json.load(f)
+for coin in wallet:
+    if coin["bought"] is not None:
+        result["earned_money"] -= Decimal(coin["bought"]) * Decimal(coin["matecoin_price"])
+        result["matecoin_account"] += Decimal(coin["bought"])
+    if coin["sold"] is not None:
+        result["earned_money"] += Decimal(coin["sold"]) * Decimal(coin["matecoin_price"])
+        result["matecoin_account"] -= Decimal(coin["sold"])
+earned = result["earned_money"]
+balance = result["matecoin_account"]
+result = {"earned_money": str(earned), "matecoin_account": str(balance)}
+
+
+with open("profit.json", "w") as file:
+    json.dump(result, file)
