df4d5bb42caf4c5bcfa00cfc33ac5daccbca9c2f
Kyryl <sautakiki.com@gmail.com>
2026-02-12 21:10:35 +0200

    sol

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import json
 from decimal import Decimal
 
+
 def calculate_profit() -> None:
     result = {"earned_money": Decimal("0"),
               "matecoin_account": Decimal("0")}
@@ -17,7 +18,5 @@ def calculate_profit() -> None:
     earned = result["earned_money"]
     balance = result["matecoin_account"]
     result = {"earned_money": str(earned), "matecoin_account": str(balance)}
-
-
     with open("profit.json", "w") as file:
         json.dump(result, file)
