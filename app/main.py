34281d9b1120027830057a1179db9b59416b15c6
Dmytro Ostapenko <dr.dm.ostapenko@gmail.com>
2026-02-18 11:39:56 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 import json
 from decimal import Decimal
 from pathlib import Path
+>>>>>>> dfbf2b4
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+=======
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+>>>>>>> dfbf2b4 (Solution)
 TRADES = f"{BASE_DIR}/app/trades.json"
 PROFIT = f"{BASE_DIR}/profit.json"
 
@@ -22,6 +28,7 @@ def calculate_profit(filename: str) -> None:
         else:
             trades = []
 
+<<<<<<< HEAD
         for trade in trades:
             price = Decimal(str(trade.get("matecoin_price") or "0"))
             if trade.get("bought") not in (None, ""):
@@ -41,3 +48,24 @@ def calculate_profit(filename: str) -> None:
 
         with open("profit.json", "w") as json_file:
             json.dump(result, json_file, indent=2, ensure_ascii=False)
+=======
+    for trade in trades:
+        price = Decimal(str(trade.get("matecoin_price") or "0"))
+        if trade.get("bought") not in (None, ""):
+            bought = Decimal(str(trade.get("bought")))
+            earned_money -= bought * price
+            matecoin_account += bought
+
+        if trade.get("sold") not in (None, ""):
+            sold = Decimal(str(trade.get("sold")))
+            earned_money += sold * price
+            matecoin_account -= sold
+
+    result = {
+        "earned_money": str(earned_money),
+        "matecoin_account": str(matecoin_account),
+    }
+
+    with open("profit.json", "w") as json_file:
+        json.dump(result, json_file, indent=2, ensure_ascii=False)
+>>>>>>> dfbf2b4 (Solution)
