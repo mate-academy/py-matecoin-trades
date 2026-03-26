7693e2321d7c7f048072e2d620c535cef575cb19
kuligambino01 <czaro923@gmail.com>
2026-03-26 11:53:01 +0100

    'solved_flake8v2'

--- app/main.py
+++ app/main.py
@@ -11,17 +11,23 @@ def calculate_profit(file_name: str) -> None:
 
     for data in conv_dict:
         if data["bought"] is not None:
-            money_balance -= (Decimal(data["bought"]) *
-                              Decimal(data["matecoin_price"]))
+            money_balance -= (
+                Decimal(data["bought"])
+                * Decimal(data["matecoin_price"])
+            )
             coin_balance += Decimal(data["bought"])
 
         if data["sold"] is not None:
-            money_balance += (Decimal(data["sold"]) *
-                              Decimal(data["matecoin_price"]))
+            money_balance += (
+                Decimal(data["sold"])
+                * Decimal(data["matecoin_price"])
+            )
             coin_balance -= Decimal(data["sold"])
 
-    bank = {"earned_money": str(money_balance),
-            "matecoin_account": str(coin_balance)}
+    bank = {
+        "earned_money": str(money_balance),
+        "matecoin_account": str(coin_balance),
+    }
 
     with open("profit.json", "w") as file:
         json.dump(bank, file, indent=2)
