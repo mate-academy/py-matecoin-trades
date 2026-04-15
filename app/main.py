67a7276dbf9413c09a9b20b5003c1c45d08b4bb9
Samuel Hurtado <samuelhurtado20@hotmail.com>
2026-04-15 17:52:40 -0500

    Remove unnecessary blank lines in main.py

--- app/main.py
+++ app/main.py
@@ -13,20 +13,19 @@ def calculate_profit(trades_file_path: str) -> None:
     # 2. Procesar cada transacción
     for trade in trades:
         price = Decimal(trade["matecoin_price"])
-        
+
         if trade["bought"] is not None:
             bought_volume = Decimal(trade["bought"])
             # Comprar resta dinero y suma monedas
             earned_money -= bought_volume * price
             matecoin_account += bought_volume
-            
+
         if trade["sold"] is not None:
             sold_volume = Decimal(trade["sold"])
             # Vender suma dinero y resta monedas
             earned_money += sold_volume * price
             matecoin_account -= sold_volume
 
-    # 3. Preparar el resultado (todo como string según el requisito)
     result = {
         "earned_money": str(earned_money),
         "matecoin_account": str(matecoin_account)
