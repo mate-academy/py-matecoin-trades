d22f67880c7fb89c272c629566a75be80e2260e4
alemonnjy222 <71006485+alemonnjy222@users.noreply.github.com>
2026-05-10 11:59:50 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,27 +1,17 @@
 import json
 from decimal import Decimal
-from typing import Any
-
-
-def from_json_to_dict(file_path: str) -> Any:
-    with open(file_path, "r") as file:
-        data = json.load(file)
-    return data
-
-
-def from_dict_to_json(file_path: str, data: Any) -> None:
-    with open(file_path, "w") as file:
-        json.dump(data, file, indent=2)
 
 
 def calculate_profit(file_path: str) -> None:
-    trades = from_json_to_dict(file_path)
+    with open(file_path, "r") as file:
+        trades = json.load(file)
 
     balance_usd = Decimal("0")
     balance_coin = Decimal("0")
 
     for trade in trades:
         price = Decimal(trade["matecoin_price"])
+
         if trade.get("bought") is not None:
             bought = Decimal(trade["bought"])
             balance_usd -= bought * price
@@ -36,4 +26,6 @@ def calculate_profit(file_path: str) -> None:
         "earned_money": str(balance_usd),
         "matecoin_account": str(balance_coin)
     }
-    from_dict_to_json("profit.json", result)
+
+    with open("profit.json", "w") as file:
+        json.dump(result, file, indent=2)
