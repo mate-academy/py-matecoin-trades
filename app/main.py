98f311a4254769c5d6cb486c2bda4c9ede8f741f
fanghey <2603Lysenko2603@gmail.com>
2026-06-17 17:32:29 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,8 +8,8 @@ def calculate_profit(filename: str) -> None:
     with open(filename, "r", encoding="utf-8") as f:
         trades = json.load(f)
 
-    total_bought_coins = Decimal("0")
-    total_sold_coins = Decimal("0")
+    total_bought = Decimal("0")
+    total_sold = Decimal("0")
     earned_money = Decimal("0")
 
     for trade in trades:
@@ -17,15 +17,15 @@ def calculate_profit(filename: str) -> None:
 
         if trade["bought"] is not None:
             bought = Decimal(trade["bought"])
-            total_bought_coins += bought
-            earned_money -= bought * price  # покупка = расход денег
+            total_bought += bought
+            earned_money -= bought * price
 
         if trade["sold"] is not None:
             sold = Decimal(trade["sold"])
-            total_sold_coins += sold
-            earned_money += sold * price  # продажа = доход
+            total_sold += sold
+            earned_money += sold * price
 
-    balance = total_bought_coins - total_sold_coins
+    balance = total_bought - total_sold
 
     result = {
         "earned_money": str(earned_money),
@@ -33,4 +33,4 @@ def calculate_profit(filename: str) -> None:
     }
 
     with open("profit.json", "w", encoding="utf-8") as f:
-        json.dump(result, f, ensure_ascii=False)
+        json.dump(result, f, ensure_ascii=False, indent=2)
