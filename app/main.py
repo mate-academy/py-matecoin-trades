b7f22fce132e0a870eb63d240fe8dd428a367dce
Krastidney <mizukanovanadya@gmail.com>
2026-07-20 19:42:51 +0300

    Refactor profit calculation logic in main.py

--- app/main.py
+++ app/main.py
@@ -5,26 +5,24 @@ from decimal import Decimal
 def calculate_profit(file_name: str) -> None:
     with open(file_name) as f:
         data = json.load(f)
+        balance_coins = Decimal("0")
+        earned_coins = Decimal("0")
+        for trade in data:
+            coins_price = Decimal(trade["matecoin_price"])
+            if trade["bought"] is not None:
+                bought_coins = Decimal(trade["bought"])
+                earned_coins -= bought_coins * coins_price
+                balance_coins += bought_coins
 
-    bought_coins = Decimal("0")
-    sold_coins = Decimal("0")
-    total_costs = Decimal("0")
-    total_sales = Decimal("0")
+            if trade["sold"] is not None:
+                sold_coins = Decimal(trade["sold"])
+                earned_coins += sold_coins * coins_price
+                balance_coins -= sold_coins
 
-    for trade in data:
-        metacoins_price = Decimal(str(trade["matecoin_price"]))
-        if trade["bought"]:
-            num_bought_coins = Decimal(str(trade["bought"]))
-            bought_coins += num_bought_coins
-            total_costs += metacoins_price * num_bought_coins
-        if trade["sold"]:
-            num_sold_coins = Decimal(str(trade["sold"]))
-            sold_coins += num_sold_coins
-            total_sales -= metacoins_price * num_sold_coins
-
-    result = {}
-    result["earned_money"] = str(-total_costs - total_sales)
-    result["matecoin_account"] = str(bought_coins - sold_coins)
+    result = {
+        "earned_money": str(earned_coins),
+        "matecoin_account": str(balance_coins)
+    }
 
     with open("profit.json", "w") as p:
         json.dump(result, p, indent=2)
