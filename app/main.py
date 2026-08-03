ccebad9e039996159b967a1b77abd34b2abe4a0b
Andrii Zelenskyi <Zelenskiy2013@gmail.com>
2026-08-03 22:32:19 +0300

    fix according flake app

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 import json
 from decimal import Decimal
 
+
 filename = "trades.json"
 
-def calculate_profit(filename: str):
+
+def calculate_profit(filename: str) -> None:
     with open(filename) as f:
         data = json.load(f)
     total_buy = Decimal("0")
@@ -13,13 +15,15 @@ def calculate_profit(filename: str):
     for elem in data:
         if elem["bought"]:
             total_buy += Decimal(elem["bought"])
-            money_buy += Decimal(elem["bought"]) * Decimal(elem["matecoin_price"])
+            money_buy += (Decimal(elem["bought"])
+                          * Decimal(elem["matecoin_price"]))
         if elem["sold"]:
             total_sold += Decimal(elem["sold"])
-            money_sold += Decimal(elem["sold"]) * Decimal(elem["matecoin_price"])
+            money_sold += (Decimal(elem["sold"])
+                           * Decimal(elem["matecoin_price"]))
     matecoin_account = total_buy - total_sold
     earned_money = money_sold - money_buy
-    output_data = {"earned_money": str(earned_money), "matecoin_account": str(matecoin_account)}
+    output_data = {"earned_money": str(earned_money),
+                   "matecoin_account": str(matecoin_account)}
     with open("profit.json", "w") as file:
         json.dump(output_data, file, indent=2)
-
