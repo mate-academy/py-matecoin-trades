74310c1b1c1a0f0f848a48b8551af0e23c17b98f
Rafael Carvalho <dannyelrc@hotmail.com>
2026-08-03 17:14:00 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,46 @@
-# write your code here
+import json
+from decimal import Decimal
+
+
+def calculate_profit(trades: str) -> None:
+
+    with open(trades) as trades_file:
+        trades_operations = json.load(trades_file)
+
+    operations_dict = {}
+    earned_money = 0
+    matecoin_balance = 0
+
+    for operation in trades_operations:
+        if operation["sold"] is None:
+            op_balance = Decimal(operation["bought"])
+            earned_money -= (
+                Decimal(op_balance) * Decimal(operation["matecoin_price"])
+            )
+            matecoin_balance += op_balance
+
+        elif operation["bought"] is None:
+            op_balance = Decimal(operation["sold"])
+            earned_money += (
+                Decimal(op_balance) * Decimal(operation["matecoin_price"])
+            )
+            matecoin_balance -= op_balance
+
+        else:
+            earned_money -= (
+                Decimal(operation["bought"])
+                * Decimal(operation["matecoin_price"])
+            )
+            earned_money += (
+                Decimal(operation["sold"])
+                * Decimal(operation["matecoin_price"])
+            )
+            matecoin_balance += (
+                Decimal(operation["bought"]) - Decimal(operation["sold"])
+            )
+
+    operations_dict["earned_money"] = str(earned_money)
+    operations_dict["matecoin_account"] = str(matecoin_balance)
+
+    with open("profit.json", "w") as file:
+        json.dump(operations_dict, file, indent=2)
