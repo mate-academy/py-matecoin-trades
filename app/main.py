361596957435d2c04df36b1168e89ff3006aaa1e
Alexey Gridenko <agridenko@gmail.com>
2026-08-11 15:47:41 +0300

    added functional in task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,34 @@
-# write your code here
+import json
+from decimal import Decimal
+
+
+def calculate_profit(filename: str) -> None:
+    with open(filename, "r") as file:
+        trades = json.load(file)
+
+    earned_money = Decimal("0")
+    matecoin_account = Decimal("0")
+
+    for trade in trades:
+        price = Decimal(trade["matecoin_price"])
+
+        if trade["bought"]:
+            bought = Decimal(trade["bought"])
+            earned_money -= bought * price
+            matecoin_account += Decimal(trade["bought"])
+
+        if trade["sold"]:
+            sold = Decimal(trade["sold"])
+            earned_money += sold * price
+            matecoin_account -= Decimal(trade["sold"])
+
+    result = {
+        "earned_money": str(earned_money),
+        "matecoin_account": str(matecoin_account)
+    }
+
+    with open("profit.json", "w") as f:
+        json.dump(result, f, indent=2)
+
+
+calculate_profit("app/trades.json")
