77e8222bd62442469b861b6b6f20dc8fd454e31b
Krzysztof Sobczak <krzysztof.sobczak5081@gmail.com>
2026-08-27 10:04:37 +0200

    Read json

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+from json import dump, loads
+
+
+def calculate_profit(file_name: str) -> None:
+    with open(file_name) as json_file:
+        object = loads(json_file.read())
+
+print(object)
