36d0a12c6f8fbab838341ceaaa7219816f6ad2f2
Krzysztof Sobczak <krzysztof.sobczak5081@gmail.com>
2026-08-27 10:09:24 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 from json import loads
 
-result = []
 
 def calculate_profit(file_name: str) -> None:
     with open(file_name) as json_file:
         result = loads(json_file.read())
-
-print(result)
+        print(result)
