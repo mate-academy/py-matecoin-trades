c320df0c0b42cacf122c67f76c152d254a47578c
Krzysztof Sobczak <krzysztof.sobczak5081@gmail.com>
2026-08-27 10:58:54 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
 from json import loads
+from decimal import *
 
 
 def calculate_profit(file_name: str) -> None:
+    getcontext().prec = 5
+    trades = []
+    earned_money = Decimal(0)
+    matecoin_account = Decimal(0)
+    
     with open(file_name) as json_file:
-        result = loads(json_file.read())
-        print(result)
+        trades = loads(json_file.read())
+
+    for trade in trades:
+        price = Decimal(trade["matecoin_price"])
+        if trade["bought"]:
+            bought = Decimal(trade["bought"])
+            earned_money -= Decimal(trade["bought"]) * price
+            matecoin_account += 
+        if trade["sold"]:
+            sell = Decimal(trade["sold"])
+            
+        
